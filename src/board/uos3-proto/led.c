343c4ff808bd31994744338956f4af9b883ccffe
Phil Crump <phil@philcrump.co.uk>
2017-06-11 22:29:07 +0100

    src: Usability tweaks.

--- src/board/uos3-proto/led.c
+++ src/board/uos3-proto/led.c
@@ -4,75 +4,74 @@
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 
+/* LED description struct */
 typedef struct LED {
-  uint32_t peripheral;
-  uint32_t port;
-  uint8_t  pin;
+  uint32_t peripheral;  // TI Driver Peripheral Reference
+  uint32_t port;        // TI Driver Port Reference
+  uint8_t  pin;         // TI Driver Pin Reference
   bool initialised;
-  enum { state_ON
-       , state_OFF
-       } state;
+  bool state;
 } LED;
 
+/* Array of connected LEDs */
 static LED LED_leds[2] = 
-  { { SYSCTL_PERIPH_GPIOC, GPIO_PORTC_BASE, GPIO_PIN_2, false, state_OFF } // LED A
-  , { SYSCTL_PERIPH_GPIOB, GPIO_PORTB_BASE, GPIO_PIN_1, false, state_OFF } // LED B
+  { { SYSCTL_PERIPH_GPIOC, GPIO_PORTC_BASE, GPIO_PIN_2, false, false } // LED A
+  , { SYSCTL_PERIPH_GPIOB, GPIO_PORTB_BASE, GPIO_PIN_1, false, false } // LED B
   };
 #define NUMBER_OF_LEDS  ( sizeof(LED_leds) / sizeof(LED) )
 
+/* Initialise Hardware for an LED */
 static void LED_init(LED *led)
 {
+  /* Check LED is initialised, exit if it is */
   if(led->initialised)
     return;
 
-  // Initialise Peripheral
+  /* Initialise Peripheral if not already initialised */
   if(!SysCtlPeripheralReady(led->peripheral))
   {
     SysCtlPeripheralEnable(led->peripheral);
     while(!SysCtlPeripheralReady(led->peripheral)) { };
   }
 
-  // Initialise Pin
+  /* Initialise LED Pin on Port */
   GPIOPinTypeGPIOOutput(led->port, led->pin);
 
   led->initialised = true;
 }
 
+/** Public Functions */
+
 void LED_on(uint8_t led_num)
 {
-  if(led_num >= NUMBER_OF_LEDS)
-    return;
-
-  LED_init(&LED_leds[led_num]);
-
-  GPIOPinWrite(LED_leds[led_num].port, LED_leds[led_num].pin, LED_leds[led_num].pin);
-
-  LED_leds[led_num].state = state_ON;
+  LED_set(led_num, true);
 }
 
 void LED_off(uint8_t led_num)
 {
-  if(led_num >= NUMBER_OF_LEDS)
-    return;
-  
-  LED_init(&LED_leds[led_num]);
-
-  GPIOPinWrite(LED_leds[led_num].port, LED_leds[led_num].pin, 0);
-
-  LED_leds[led_num].state = state_OFF;
+  LED_set(led_num, false);
 }
+
+/* Abstract for weird TI third argument */
+#define LED_Pin_Write(led_num, pin_value)  GPIOPinWrite(LED_leds[led_num].port, LED_leds[led_num].pin, pin_value)
 
 void LED_set(uint8_t led_num, bool state)
 {
   if(led_num >= NUMBER_OF_LEDS)
     return;
-  
-  if(state==true)
+
+  LED_init(&LED_leds[led_num]);
+
+  /* Write value to LED pin, abstracted to macro above */
+  /* - second argument is for LOW: 0, for HIGH: LED_leds[led_num].pin */
+  if(state == true)
   {
-    LED_on(led_num);
+    LED_Pin_Write(led_num, LED_leds[led_num].pin);
   }
   else
   {
-    LED_off(led_num);
+    LED_Pin_Write(led_num, 0);
   }
+
+  LED_leds[led_num].state = state;
 }
--- src/firmware.h
+++ src/firmware.h
@@ -3,6 +3,9 @@
 #ifndef __FIRMWARE_H__
 #define __FIRMWARE_H__
 
+#include <stdint.h>
+#include <stdbool.h>
+
 /* Board-specific header file, completed by macro: 'board/<board>/board.h' */
 #include BOARD_INCLUDE
 
