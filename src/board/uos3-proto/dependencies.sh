101400c20dbf36d2ef9ccd313c399ef7eb6500ad
Phil Crump <phil@philcrump.co.uk>
2017-07-18 17:08:18 +0100

    build: Move compiler check to board dependencies.

--- src/board/uos3-proto/dependencies.sh
+++ src/board/uos3-proto/dependencies.sh
@@ -34,6 +34,19 @@ print_err() {
     fi
 }
 
+compiler=arm-none-eabi-gcc
+
+## Check Compiler is installed
+hash ${compiler} 2>/dev/null \
+|| {
+    print_err "$_ERROR_ GNU ARM Embedded Compiler not installed! Aborting.";
+    print_err "$_ERROR_ Please install '${compiler}'";
+    exit 1;
+}
+
+## Print compiler version
+print "$_INFO_ Using GNU ARM Embedded $(${compiler} --version | head -n1)"
+
 tm4c_version="2.1.4.178"
 tm4c_dir="TivaWare_C_Series-${tm4c_version}"
 tm4c_driver="${tm4c_dir}/driverlib/gcc/libdriver.a"
@@ -43,28 +56,30 @@ if [ ! -e "${tm4c_driver}" ]; then
   ## Check for TM4C Driver source directory
   if [ -d "${tm4c_dir}" ]; then
     ## source directory exists so compile Driver Blob
-    print "$_INFO_ Compiling TivaWare Driver.."
+    print "$_INFO_ Compiling TI TivaWare Driver.."
     cd ${tm4c_dir}/driverlib
     make &>/dev/null \
     && [ -e "${tm4c_driver}" ] \
     && {
-      print "$_INFO_ TivaWare Driver Compiled.";
+      print "$_INFO_ TI TivaWare Driver Compiled.";
     } || {
-      print_err "$_ERROR_ There were errors in building the TivaWare Driver :(";
+      print_err "$_ERROR_ There were errors in building the TI TivaWare Driver :(";
       exit 1;
     }
   ## Check for downloadable TM4C Driver Blob
   elif [ ! -z "$TM4C_LIBDRIVER_URL" ]; then
     ## URL is set on environment variable so download
-    print "$_INFO_ Downloading TivaWare Driver.."
+    print "$_INFO_ Downloading TI TivaWare Driver.."
     wget -q "$TM4C_LIBDRIVER_URL-${tm4c_version}.tar.bz2" -O tm4c.tar.bz2 \
     && tar -xjf tm4c.tar.bz2 && rm tm4c.tar.bz2 \
-    && print "$_INFO_ TivaWare Driver Downloaded."
+    && print "$_INFO_ TI TivaWare Driver Downloaded."
   else
-    print_err "$_ERROR_ TivaWare Driver Library not found! Please download these from TI following README.md"
+    print_err "$_ERROR_ TI TivaWare Driver Library not found! Aborting.";
+    print_err "$_ERROR_ Please download the library from TI following README.md"
     exit 1
   fi
 fi
 
-print "$_INFO_ Using TivaWare Driver Library ${tm4c_version}";
+## Print TI TivaWare version
+print "$_INFO_ Using TI TivaWare Driver Library ${tm4c_version}";
 exit 0;
