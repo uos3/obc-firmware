01784cb30dc07aab4e21f396709e3c55de7fd79d
Suz <rflowers666@gmail.com>
2017-08-04 12:42:32 +0100

    merged serial test with TI i2c code, using blinky terminal, currently building, now to check I2C addresses

--- src/main/i2ctest.c
+++ src/main/i2ctest.c
@@ -6,76 +6,7 @@
 /* firmware.h contains all relevant headers */
 #include "../firmware.h"
 
-/* A very simple example that blinks the on-board LED. */
-
-int main(void)
-{
-  Board_init();
-  WDT_kick();
-
-  while(1)
-  {
-    LED_on(LED_B);
-
-    /* On period */
-    Delay_ms(500);
-    WDT_kick();
-
-    LED_off(LED_B);
-
-    /* Off period */
-    Delay_ms(500);
-    WDT_kick();
-  }
-}
-
-// hack it to work with gyro and satellite i2c system (WIP)
-
-// introduce below step by step (currently not even linking properly)
-
-/*
-
-//*****************************************************************************
-//
-// master_slave_loopback.c - Example demonstrating a simple I2C message
-//                           transmission and reception.
-//
-// Copyright (c) 2010-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-//   Redistribution and use in source and binary forms, with or without
-//   modification, are permitted provided that the following conditions
-//   are met:
-// 
-//   Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// 
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the  
-//   distribution.
-// 
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-// This is part of revision 2.1.4.178 of the Tiva Firmware Development Package.
-//
-//*****************************************************************************
-
-#include "../firmware.h" // this is bringing in satellite links, standard ones not configured for this build setting currently
+#include "i2ctest.h" // local header
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -87,7 +18,42 @@ int main(void)
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
-#include "utils/uartstdio.h"
+
+/* A very simple example that blinks the on-board LED. */
+
+/*
+
+int main(void)
+{
+  Board_init();
+  WDT_kick();
+
+  UART_init(UART_CAM_HEADER, 9600);
+
+  while(1)
+  {
+    LED_on(LED_B); 
+UART_puts(UART_CAM_HEADER, "LED On\r\n");
+    Delay_ms(500);
+    WDT_kick();
+
+    LED_off(LED_B);
+UART_puts(UART_CAM_HEADER, "LED Off\r\n");
+     
+    Delay_ms(500);
+    WDT_kick();
+  }
+}
+
+*/
+
+void UARTprintf(x)               // lets use our console instead of TI one
+ {UART_puts(UART_CAM_HEADER,x);UART_puts(UART_CAM_HEADER,"\r");}
+
+// hack it to work with gyro and satellite i2c system (WIP)
+
+// introduce below step by step (currently not even linking properly)
+
 
 //*****************************************************************************
 //
@@ -143,50 +109,8 @@ int main(void)
 //*****************************************************************************
 #define SLAVE_ADDRESS 0x3C
 
-//*****************************************************************************
-//
-// This function sets up UART0 to be used for a console to display information
-// as the example is running.
-//
-//*****************************************************************************
-void
-InitConsole(void)
-{
-    //
-    // Enable GPIO port A which is used for UART0 pins.
-    // TODO: change this to whichever GPIO port you are using.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    //
-    // Configure the pin muxing for UART0 functions on port A0 and A1.
-    // This step is not necessary if your part does not support pin muxing.
-    // TODO: change this to select the port/pin you are using.
-    //
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
 
-    //
-    // Enable UART0 so that we can configure the clock.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-
-    //
-    // Use the internal 16MHz oscillator as the UART clock source.
-    //
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-
-    //
-    // Select the alternate (UART) function for these pins.
-    // TODO: change this to select the port/pin you are using.
-    //
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    //
-    // Initialize the UART for console I/O.
-    //
-    UARTStdioConfig(0, 115200, 16000000);
-}
 
 //*****************************************************************************
 //
@@ -306,7 +230,15 @@ main(void)
     // Set up the serial console to use for displaying messages.  This is
     // just for this example program and is not needed for I2C operation.
     //
-    InitConsole();
+
+  Board_init();
+  WDT_kick();
+
+  UART_init(UART_CAM_HEADER, 9600);
+
+
+
+  //  InitConsole();
 
     //
     // Display the example setup on the console.
@@ -461,4 +393,4 @@ main(void)
     return(0);
 }
 
-*/
+
---
+++ src/main/i2ctest.h
@@ -0,0 +1,5 @@
+// i2ctest.h
+
+// function prototypes for i2ctest.c
+
+extern void InitConsole(void);
