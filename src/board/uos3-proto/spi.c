c294129471611a5e5004336a37b8c37ac6efb2b6
Phil Crump <phil@philcrump.co.uk>
2017-08-18 22:35:36 +0100

    uos3-proto/spi: Driver fixes.

--- src/board/uos3-proto/spi.c
+++ src/board/uos3-proto/spi.c
@@ -154,13 +154,9 @@ uint8_t SPI_cmdstrobe(uint8_t spi_num, uint8_t cmd)
   GPIO_write(spi->gpio_cs, false);
 
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
-  while(SSIBusy(spi->port->base_spi)) {};
 
   /* Send Command */
   SSIDataPut(spi->port->base_spi, cmd);
-  while(SSIBusy(spi->port->base_spi)) {};
-
-  /* Read return status */
   SSIDataGet(spi->port->base_spi, &readValue);
 
   GPIO_write(spi->gpio_cs, true);
@@ -182,17 +178,13 @@ uint8_t SPI_read8(uint8_t spi_num, uint8_t addr, uint8_t *data)
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
   /* Transmit address with Read Bit Set */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x80|addr));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
 
+  /* Read Data */
   SSIDataPut(spi->port->base_spi, 0);
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &d);
   *data = (uint8_t)d;
 
@@ -216,19 +208,14 @@ uint8_t SPI_burstread8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint16_t le
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
   /* Transmit address with Read Bit Set */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x80|addr));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
 
   for(uint32_t i = 0; i < len; i++)
   {
     SSIDataPut(spi->port->base_spi, 0);
-    while(SSIBusy(spi->port->base_spi)) {};
-
     SSIDataGet(spi->port->base_spi, &d);
     *data++ = (uint8_t)d;
   }
@@ -253,18 +240,14 @@ uint8_t SPI_write8(uint8_t spi_num, uint8_t addr, uint8_t *data)
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
   /* Transmit address without Read Bit */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x7F & addr));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
 
+  /* Transmit data */
   w = *data;
   SSIDataPut(spi->port->base_spi, w);
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &d);
 
   GPIO_write(spi->gpio_cs, true);
@@ -286,21 +269,16 @@ uint8_t SPI_burstwrite8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint16_t l
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
   /* Transmit address without Read Bit */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x7F & addr));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
 
-  /* Communicate len number of bytes: if TX - the procedure doesn't overwrite data */
+  /* Transmit data */
   for (uint32_t i = 0; i < len; i++)
   {
     w = *data;
     SSIDataPut(spi->port->base_spi, w);
-    while(SSIBusy(spi->port->base_spi)) {};
-
     SSIDataGet(spi->port->base_spi, &d);
     data++;
   }
@@ -329,17 +307,16 @@ uint8_t SPI_read16(uint8_t spi_num, uint16_t addr, uint8_t *data)
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
  
-  while(SSIBusy(spi->port->base_spi));
-  /* Transmit address with Read Bit Set */
+  /* Transmit MSB address with Read Bit Set */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x80|addr_msb));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
-  while(SSIBusy(spi->port->base_spi)) {};
 
+  /* Transmit LSB address */
   SSIDataPut(spi->port->base_spi, (uint32_t)addr_lsb);
-  while(SSIBusy(spi->port->base_spi)) {};
+  SSIDataGet(spi->port->base_spi, &d);
 
+  /* Read Result */
+  SSIDataPut(spi->port->base_spi, 0);
   SSIDataGet(spi->port->base_spi, &d);
   *data = (uint8_t)d;
 
@@ -366,23 +343,18 @@ uint8_t SPI_burstread16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint16_t 
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
-  /* Transmit address with Read Bit Set */
+  /* Transmit MSB address with Read Bit Set */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x80|addr_msb));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
-  while(SSIBusy(spi->port->base_spi)) {};
 
+  /* Transmit LSB address */
   SSIDataPut(spi->port->base_spi, (uint32_t)addr_lsb);
-  while(SSIBusy(spi->port->base_spi)) {};
+  SSIDataGet(spi->port->base_spi, &d);
 
   for(uint32_t i = 0; i < len; i++)
   {
     SSIDataPut(spi->port->base_spi, 0);
-    while(SSIBusy(spi->port->base_spi)) {};
-
     SSIDataGet(spi->port->base_spi, &d);
     *data++ = (uint8_t)d;
   }
@@ -410,26 +382,17 @@ uint8_t SPI_write16(uint8_t spi_num, uint16_t addr, uint8_t *data)
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso){};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
-  /* Transmit address without Read Bit */
+  /* Transmit MSB address without Read Bit */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x7F & addr_msb));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
-  while(SSIBusy(spi->port->base_spi)) {};
 
+  /* Transmit LSB address */
   SSIDataPut(spi->port->base_spi, (uint32_t)addr_lsb);
-  while(SSIBusy(spi->port->base_spi)) {};
-
-  // do we need a dummy read?
-  //SSIDataGet(SSI1_BASE, (uint32_t *)&d);
-  //while(SSIBusy(spi->port->base_spi)) {};
+  SSIDataGet(spi->port->base_spi, &d);
 
   w = *data;
   SSIDataPut(spi->port->base_spi, w);
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &d);
 
   GPIO_write(spi->gpio_cs, true);
@@ -454,25 +417,19 @@ uint8_t SPI_burstwrite16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint16_t
   while(GPIOPinRead(spi->port->base_gpio, spi->port->pin_miso) & spi->port->pin_miso) {};
   // do we need to flush the rx buffer first?
   SPI_flush(spi->port->base_spi);
-  while(SSIBusy(spi->port->base_spi)) {};
 
-  /* Transmit address without Read Bit */
+  /* Transmit MSB address without Read Bit */
   SSIDataPut(spi->port->base_spi, (uint32_t)(0x7F & addr_msb));
-  while(SSIBusy(spi->port->base_spi)) {};
-
   SSIDataGet(spi->port->base_spi, &readValue);
-  while(SSIBusy(spi->port->base_spi)) {};
 
+  /* Transmit LSB address */
   SSIDataPut(spi->port->base_spi, (uint32_t)addr_lsb);
-  while(SSIBusy(spi->port->base_spi)) {};
+  SSIDataGet(spi->port->base_spi, &d);
 
-  /* Communicate len number of bytes: if TX - the procedure doesn't overwrite data */
   for (uint32_t i = 0; i < len; i++)
   {
     w = *data;
     SSIDataPut(spi->port->base_spi, w);
-    while(SSIBusy(spi->port->base_spi)) {};
-
     SSIDataGet(spi->port->base_spi, &d);
     data++;
   }
