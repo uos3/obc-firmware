99053447db21c03faac6fcf007210808746ff7b1
Phil Crump <phil@philcrump.co.uk>
2017-09-09 18:59:24 +0100

    packet: Add tests.

--- src/packet/packet.c
+++ src/packet/packet.c
@@ -1,21 +1,24 @@
 #include "../firmware.h"
 
-void packet_scramble_xor(uint8_t *buffer, uint32_t length)
+void packet_pn9_xor(uint8_t *buffer, uint32_t length)
 {
   /* Uses CC1120 PN9 Sequence */
 
   uint16_t state = 0xFF;
-  uint32_t i;
+  uint32_t i, j;
 
   if(length < 1)
     return;
 
-  state = 0x00FF;
+  state = 0x01FF;
   buffer[0] ^= (uint8_t)state;
 
   for(i=1; i<length; i++)
   {
-    state = (uint16_t)((state >> 1) + ((((state & 0x01) ^ (state >> 5)) & 0x01) << 8));
+    for(j=0; j<8; j++)
+    {
+      state = (uint16_t)((state >> 1) + ((((state & 0x01) ^ (state >> 5)) & 0x01) << 8));
+    }
 
     buffer[i] ^= (uint8_t)state;
   }
@@ -23,9 +26,13 @@ void packet_scramble_xor(uint8_t *buffer, uint32_t length)
 
 void packet_interleave(uint8_t *in, uint8_t *out, uint32_t x, uint32_t y)
 {
-  (void) in;
-  (void) out;
+  uint32_t i, j;
 
-  (void) x;
-  (void) y;
+  for(i=0; i<x; i++)
+  {
+    for(j=0; j<y; j++)
+    {
+      out[(i*y)+j] = in[(j*x)+i];
+    }
+  }
 }
--- src/packet/packet.h
+++ src/packet/packet.h
@@ -14,8 +14,10 @@ typedef struct coded_data { //1048 bits
   uint16_t crc;
 } coded_data;
 
-void packet_scramble_xor(uint8_t *buffer, uint32_t length);
+void packet_pn9_xor(uint8_t *buffer, uint32_t length);
 
 void packet_interleave(uint8_t *in, uint8_t *out, uint32_t x, uint32_t y);
+
+#define	packet_uninterleave(a, b, x, y)		packet_interleave(a, b, y, x)
 
 #endif /* __PACKET_H__ */
