71e4dbe5d5399ac1096527230dc3ac7c629abe77
Phil Crump <phil@philcrump.co.uk>
2017-09-17 12:04:22 +0100

    doxygen: Tweaks (10)

--- tools/travis_doxygen_docs.sh
+++ tools/travis_doxygen_docs.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env /bin/bash
 
-source_dir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
-cd "$source_dir";
+COLOUR_GREEN='\033[0;32m'
+COLOUR_YELLOW='\033[1;33m'
+COLOUR_PURPLE='\033[0;35m'
+COLOUR_RED='\033[0;31m'
+COLOUR_OFF='\033[0;00m'
+CLEAR_LINE='\033[2K'
+
+_ERROR_="$COLOUR_RED[ERROR]$COLOUR_OFF"
+_INFO_="$COLOUR_PURPLE[INFO]$COLOUR_OFF"
+_DEBUG_="$COLOUR_YELLOW[DEBUG]$COLOUR_OFF"
 
 # Exit with nonzero exit code if anything fails
 set -e
 
-echo 'Decrypting SSH key'
+# Change to script source directory
+source_dir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+cd "$source_dir";
+
+print "$_DEBUG_ Decrypting SSH key"
 # Get the deploy key by using Travis's stored variables to decrypt travis-deploy.enc
 openssl aes-256-cbc -iv ${DEPLOY_KEY_IV} -K ${DEPLOY_KEY_KEY} -in travis-deploy.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-echo 'Cloning repository'
+print "$_DEBUG_ Cloning repository"
 # Get the current gh-pages branch
 git clone -b gh-pages ssh://git@github.com:/${TRAVIS_REPO_SLUG} gh-pages
 cd gh-pages
 
-echo 'Configuring git'
+print "$_DEBUG_ Configuring git"
 # Set the push default to simple i.e. push only the current branch.
 git config --global push.default simple
 # Pretend to be an user called Travis CI.
@@ -26,55 +38,29 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
 # Remove everything currently in the gh-pages branch.
-# GitHub is smart enough to know which files have changed and which files have
-# stayed the same and will only update the changed files. So the gh-pages branch
-# can be safely cleaned, and it is sure that everything pushed later is the new
-# documentation.
 rm -rf *
 
 # Need to create a .nojekyll file to allow filenames starting with an underscore
-# to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
-# Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
-# to NO, which it is by default. So creating the file just in case.
 echo "" > .nojekyll
 
-################################################################################
-##### Generate the Doxygen code documentation and log the output.          #####
-echo 'Generating Doxygen code documentation...'
-
-# Redirect both stderr and stdout to the log file AND the console.
+print "$_DEBUG_ Generating Doxygen code documentation"
+# Redirect both stderr and stdout to the log file and the console.
 doxygen ../doxyfile 2>&1 | tee doxygen.log
 
 # Bail out if there's no changes
 if [ -z `git diff --exit-code` ]; then
-    echo "No doc changes on this push; exiting."
+    print "$_INFO_ No doc changes on this push; exiting."
     exit 0
 fi
 
-################################################################################
-##### Upload the documentation to the gh-pages branch of the repository.   #####
 # Only upload if Doxygen successfully created the documentation.
-# Check this by verifying that the html directory and the file html/index.html
-# both exist. This is a good indication that Doxygen did it's work.
 if [ -f "index.html" ]; then
-
-    echo 'Uploading documentation to the gh-pages branch...'
-    # Add everything in this directory (the Doxygen code documentation) to the
-    # gh-pages branch.
-    # GitHub is smart enough to know which files have changed and which files have
-    # stayed the same and will only update the changed files.
+    print "$_DEBUG_ Uploading documentation to the gh-pages branch."
     git add --all
-
-    # Commit the added files with a title and description containing the Travis CI
-    # build number and the GitHub commit reference that issued this build.
     git commit -m "Deploy to GitHub Pages: ${TRAVIS_COMMIT}"
-
-    # Push to the remote gh-pages branch.
-    # The ouput is redirected to /dev/null to hide any sensitive credential data
-    # that might otherwise be exposed.
-    git push # > /dev/null 2>&1
+    git push
+	print "$_INFO_ Doxygen successful."
 else
-    echo '' >&2
-    echo 'Warning: No documentation (html) files have been found!' >&2
+    print "$_ERROR_ No documentation (html) files have been found!"
     exit 1
 fi
