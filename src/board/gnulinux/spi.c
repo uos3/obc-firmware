c93f3ef1d9916ee34ab40a1b99f051b90979e97d
Phil Crump <phil@philcrump.co.uk>
2017-10-28 20:05:51 +0100

    src/board/gnulinux/spi: Fix function definitions to match recent changes.

--- src/board/gnulinux/spi.c
+++ src/board/gnulinux/spi.c
@@ -17,7 +17,7 @@ void SPI_init(uint8_t spi_num)
   (void) spi_num;
 }
 
-uint8_t SPI_cmdstrobe(uint8_t spi_num, uint8_t cmd)
+uint8_t SPI_cmd(uint8_t spi_num, uint8_t cmd)
 {
   (void) spi_num;
   (void) cmd;
@@ -34,7 +34,7 @@ uint8_t SPI_read8(uint8_t spi_num, uint8_t addr, uint8_t *data)
   return 0;
 }
 
-uint8_t SPI_burstread8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint16_t len)
+uint8_t SPI_burstread8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint32_t len)
 {
   (void) spi_num;
   (void) addr;
@@ -53,7 +53,7 @@ uint8_t SPI_write8(uint8_t spi_num, uint8_t addr, uint8_t *data)
   return 0;
 }
 
-uint8_t SPI_burstwrite8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint16_t len)
+uint8_t SPI_burstwrite8(uint8_t spi_num, uint8_t addr, uint8_t *data, uint32_t len)
 {
   (void) spi_num;
   (void) addr;
@@ -72,7 +72,7 @@ uint8_t SPI_read16(uint8_t spi_num, uint16_t addr, uint8_t *data)
   return 0;
 }
 
-uint8_t SPI_burstread16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint16_t len)
+uint8_t SPI_burstread16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint32_t len)
 {
   (void) spi_num;
   (void) addr;
@@ -91,7 +91,45 @@ uint8_t SPI_write16(uint8_t spi_num, uint16_t addr, uint8_t *data)
   return 0;
 }
 
-uint8_t SPI_burstwrite16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint16_t len)
+uint8_t SPI_burstwrite16(uint8_t spi_num, uint16_t addr, uint8_t *data, uint32_t len)
+{
+  (void) spi_num;
+  (void) addr;
+  (void) data;
+  (void) len;
+
+  return 0;
+}
+
+uint8_t SPI_read32(uint8_t spi_num, uint32_t addr, uint8_t *data)
+{
+  (void) spi_num;
+  (void) addr;
+  (void) data;
+
+  return 0;
+}
+
+uint8_t SPI_burstread32(uint8_t spi_num, uint32_t addr, uint8_t *data, uint32_t len)
+{
+  (void) spi_num;
+  (void) addr;
+  (void) data;
+  (void) len;
+
+  return 0;
+}
+
+uint8_t SPI_write32(uint8_t spi_num, uint32_t addr, uint8_t *data)
+{
+  (void) spi_num;
+  (void) addr;
+  (void) data;
+
+  return 0;
+}
+
+uint8_t SPI_burstwrite32(uint8_t spi_num, uint32_t addr, uint8_t *data, uint32_t len)
 {
   (void) spi_num;
   (void) addr;
