6c9d20ace0b0f2e5f83441203321889dba252473
Phil Crump <phil@philcrump.co.uk>
2017-11-05 21:51:19 +0000

    main: Add TempSensor driver demo.

---
+++ src/main/demo_tempsensors.c
@@ -0,0 +1,33 @@
+/* firmware.h contains all relevant headers */
+#include "../firmware.h"
+
+#include <stdio.h>
+
+#define UART_INTERFACE UART_CAM_HEADER
+
+int main(void)
+{
+  char output[100];
+
+  Board_init();
+
+  LED_on(LED_B);
+
+  UART_init(UART_INTERFACE, 9600);
+  UART_puts(UART_INTERFACE, "\r\nTemperature Sensors Demo\r\n");
+
+  while(1)
+  {
+    LED_on(LED_B);
+
+    sprintf(output,"PCT2075: %+.1f°C, TMP100: %+.1f°C\r\n",
+      ((double)Temperature_read_pct2075()/10),
+      ((double)Temperature_read_tmp100()/10)
+      );
+    UART_puts(UART_INTERFACE, output);
+
+    LED_off(LED_B);
+
+    Delay_ms(200);
+  }
+}
