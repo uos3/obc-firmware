d710590f1a90ba61d7781a759b72a925254b3ddd
Tomas Piga <shasaur@hotmail.co.uk>
2018-06-26 12:21:31 +0100

    Updated the config file with new parameters
    Config file found as TMTC in https://groupsite.soton.ac.uk/Academic/BLAST-cubesat/Documents/Forms/AllItems.aspx?RootFolder=%2FAcademic%2FBLAST-cubesat%2FDocuments%2FDATA%20PACKAGE%20%28Review%20Item%20Disrepancy%20edits%29%20CURRENT%2FEngineering%2FMission%20Description%20Document&FolderCTID=0x012000F5659642E01623428EAA32D5F7E646E6&View={C7870BB4-9FEA-4635-9448-FF2BD31AD23C}&InitialTabId=Ribbon%2EDocument&VisibilityContext=WSSTabPersistence
    - Some previous parameters still need revising

--- src/configuration/configuration.h
+++ src/configuration/configuration.h
@@ -17,67 +17,115 @@
 /* TODO: Move these below into the relevant modules */
 
 typedef enum {
-	tx_datarate_6kbps,
-	tx_datarate_3kbps,
-	tx_datarate_1kbps,
-	tx_datarate_05kbps,
-	tx_datarate_025kbps
+	tx_interval_downlink_0ms,
+	tx_interval_downlink_500ms,
+	tx_interval_downlink_600ms,
+	tx_interval_downlink_700ms,
+	tx_interval_downlink_800ms,
+	tx_interval_downlink_900ms,
+	tx_interval_downlink_1000ms,
+	tx_interval_downlink_1500ms,
+	tx_interval_downlink_2000ms
+} tx_interval_downlink_t;
+
+typedef enum {
+	tx_datarate_0_25kbps,
+	tx_datarate_0_5kbps,
+	tx_datarate_1_kbps,
+	tx_datarate_3_kbps,
+	tx_datarate_6_kbps
 } tx_datarate_t;
 
 typedef enum {
-	tx_power_300mw, 
-	tx_power_200mw, 
-	tx_power_100mw, 
-	tx_power_50mw, 
-	tx_power_10mw
+	tx_power_10mw,
+	tx_power_50mw,
+	tx_power_100mw,
+	tx_power_200mw,
+	tx_power_300mw
 } tx_power_t;
 
 typedef enum {
-	image_acquisition_profile_1600x1200_10,
-	image_acquisition_profile_640x480_10
+	image_acquisition_profile_1600x1200,
+	image_acquisition_profile_640x480
 } image_acquisition_profile_t;
 
-/* Refer to https://docs.google.com/spreadsheets/d/1OFgH6SIJJw-ISLHxVPTY4VEa49qBMccs4Qu1e_zjqvg/ */
+typedef enum {
+	operational_mode_deployment,
+	operational_mode_nominal,
+	operational_mode_safe
+} operational_mode_t;
+
+/* Refer to https://groupsite.soton.ac.uk/Academic/BLAST-cubesat/_layouts/xlviewer.aspx?id=/Academic/BLAST-cubesat/Documents/DATA%20PACKAGE%20ATRR/UoS3_SWCONFIG_%2020180618_v1.0.xlsx&Source=https%3A%2F%2Fgroupsite%2Esoton%2Eac%2Euk%2FAcademic%2FBLAST-cubesat%2FDocuments%2FForms%2FAllItems%2Easpx%3FRootFolder%3D%252FAcademic%252FBLAST%252Dcubesat%252FDocuments%252FDATA%2520PACKAGE%2520ATRR%26FolderCTID%3D0x012000F5659642E01623428EAA32D5F7E646E6%26View%3D%7BC7870BB4-9FEA-4635-9448-FF2BD31AD23C%7D%26InitialTabId%3DRibbon%252EDocument%26VisibilityContext%3DWSSTabPersistence&DefaultItemOpen=1 */
 /* Packing work in progress */
 typedef struct configuration_data_t {
-	/* 32b word */
 	bool tx_enable:1;
-	bool enable_gps_time_sync:1;
+
+	unsigned tx_interval : 8;
+	tx_interval_downlink_t tx_interval_downlink : 8;
+
+	tx_datarate_t tx_datarate : 4;
+	tx_power_t tx_power : 4;
+
+	// Upper temperature limits before entering low-power mode
+	unsigned tx_overtemp : 8;
+	unsigned rx_overtemp : 8;
+	unsigned batt_overtemp : 8;
+	unsigned obc_overtemp : 8;
+	unsigned pa_overtemp : 8;
+
+	// Battery voltage
+	unsigned low_voltage_threshold : 8;
+	unsigned low_voltage_recovery : 8;
+
+	// Time intervals for sampling and saving
+	unsigned health_acquisition_interval : 16;
+	unsigned configuration_acquisition_interval : 16;
+
+	// Inertial Measurement paramters
+	unsigned imu_acquisition_interval : 16;
+	unsigned imu_sample_count : 4;
+	unsigned imu_sample_interval : 8;
+
+	// GPS measurement parameters
+	unsigned gps_acquisition_interval : 16;
+	unsigned gps_sample_count : 4;
+	unsigned gps_sample_interval : 8;
+
+	// Camera settings
+	unsigned image_acquisition_time : 32;
+	image_acquisition_profile_t image_acquisition_profile : 1;
+
+	unsigned time : 32; //unix epoch
+
+	// Transfer between modes
+	operational_mode_t operational_mode : 2;
+	unsigned self_test : 1;
+
+	// Manually power on all rails apart form 4 which is the on-board computer
+	unsigned power_rail_1 : 1;
+	unsigned power_rail_2 : 1;
+	unsigned power_rail_3 : 1;
+	unsigned power_rail_5 : 1;
+	unsigned power_rail_6 : 1;
+
+	// Setting to True resets one of these power rails
+	unsigned reset_power_rail_1 : 1;
+	unsigned reset_power_rail_2 : 1;
+	unsigned reset_power_rail_3 : 1;
+	unsigned reset_power_rail_4 : 1;
+	unsigned reset_power_rail_5 : 1;
+	unsigned reset_power_rail_6 : 1;
+
+	// -- Previous/need revisiting
+	// Seperate control for telecom board components in case of error
 	bool imu_accel_enabled:1;
 	bool imu_gyro_enabled:1;
 	bool imu_magno_enabled:1;
 
-	/* 32b word */
-  tx_datarate_t tx_datarate:4; 
-  tx_power_t tx_power:4;
-  tx_power_t tx_overtemp_power:4;
-  imu_bandwidth_t imu_bandwidth:4;
-  image_acquisition_profile_t	image_acquisition_profile:4;
-
-	uint8_t tx_interval;
-	uint8_t tx_interval_burst;
-
-	uint8_t tx_overtemp_threshold;
-
-	uint8_t low_voltage_threshold;
-	uint8_t low_voltage_recovery;
-
-	uint16_t health_acquisition_interval; // seconds
-	uint16_t configuration_acquisition_interval; // seconds
-	uint16_t imu_acquisition_interval; // seconds
-	uint16_t gps_acquisition_interval; // seconds
-	uint32_t image_acquisition_time; // unix epoch time
-
-	
-	uint8_t imu_sample_count;
-	uint8_t imu_sample_interval; // *10ms
-
-	uint8_t gps_sample_count;
-	uint8_t gps_sample_interval; // seconds
-
-	
+	tx_power_t tx_overtemp_power:4; // adjustable low-power for safe-mode?
 
 	uint16_t greetings_message_transmission_interval; // packets
+
 } configuration_data_t;
 
 typedef struct configuration_t {
