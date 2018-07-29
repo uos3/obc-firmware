18aa90eaeef8b9101d2f8bfd4a2e3f5fd34d45ce
Phil Crump <phil@philcrump.co.uk>
2018-07-29 20:31:59 +0100

    board/gnulinux: Add rtc functions/stubs.

---
+++ src/board/gnulinux/rtc.c
@@ -0,0 +1,55 @@
+/**
+ * @ingroup gnulinux
+ * @ingroup rtc
+ *
+ * @file rtc.c
+ *
+ * @{
+ */
+
+#include "board.h"
+#include "../rtc.h"
+
+#include <time.h>
+
+void RTC_init(void)
+{
+	return;
+}
+
+void RTC_getTime(uint32_t *time)
+{
+	struct timespec tp;
+
+	if(clock_gettime(CLOCK_REALTIME, &tp) != 0)
+	{
+		*time = 0;
+		return;
+	}
+
+	*time = ((uint32_t)tp.tv_sec);
+}
+
+void RTC_getTime_ms(uint64_t *time_ms)
+{
+	struct timespec tp;
+
+	if(clock_gettime(CLOCK_REALTIME, &tp) != 0)
+	{
+		*time_ms = 0;
+		return;
+	}
+
+	*time_ms = ((uint64_t)tp.tv_sec * 1000 + (uint64_t)tp.tv_nsec / 1000000);
+}
+
+void RTC_setTime(uint32_t *time)
+{
+	(void) time;
+
+	return;
+}
+
+/**
+ * @}
+ */
