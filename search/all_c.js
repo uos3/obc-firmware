32e44d5c24884c17f4ba207781346bab26634008
Travis CI <travis@travis-ci.org>
2018-07-29 19:54:09 +0000

    Deploy to GitHub Pages: 50f3f38ea1d4b33b830cf0a2baec761254c06f31

--- search/all_c.js
+++ search/all_c.js
@@ -5,8 +5,8 @@ var searchData=
   ['random_2ec',['random.c',['../uos3-proto_2random_8c.html',1,'']]],
   ['registersetting_5ft',['registerSetting_t',['../structregister_setting__t.html',1,'']]],
   ['rtc',['Rtc',['../group__rtc.html',1,'']]],
-  ['rtc_2ec',['rtc.c',['../rtc_8c.html',1,'']]],
-  ['rtc_5fgettime',['RTC_getTime',['../group__rtc.html#ga8eceff2645c2e0bc20820f4097190ad9',1,'RTC_getTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga8eceff2645c2e0bc20820f4097190ad9',1,'RTC_getTime(uint32_t *time):&#160;rtc.c']]],
-  ['rtc_5finit',['RTC_init',['../group__rtc.html#ga83823ddc481026aea712ce81c4621b4c',1,'RTC_init(void):&#160;rtc.c'],['../group__rtc.html#ga83823ddc481026aea712ce81c4621b4c',1,'RTC_init(void):&#160;rtc.c']]],
-  ['rtc_5fsettime',['RTC_setTime',['../group__rtc.html#ga70993ebbbb06a80b16b64f90c72cdf60',1,'RTC_setTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga70993ebbbb06a80b16b64f90c72cdf60',1,'RTC_setTime(uint32_t *time):&#160;rtc.c']]]
+  ['rtc_5fgettime',['RTC_getTime',['../group__rtc.html#ga8eceff2645c2e0bc20820f4097190ad9',1,'RTC_getTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga8eceff2645c2e0bc20820f4097190ad9',1,'RTC_getTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga8eceff2645c2e0bc20820f4097190ad9',1,'RTC_getTime(uint32_t *time):&#160;rtc.c']]],
+  ['rtc_5fgettime_5fms',['RTC_getTime_ms',['../group__rtc.html#ga8694ef69651f95118d731cfee22c6036',1,'RTC_getTime_ms(uint64_t *time_ms):&#160;rtc.c'],['../group__rtc.html#ga8694ef69651f95118d731cfee22c6036',1,'RTC_getTime_ms(uint64_t *time_ms):&#160;rtc.c'],['../group__rtc.html#ga8694ef69651f95118d731cfee22c6036',1,'RTC_getTime_ms(uint64_t *time_ms):&#160;rtc.c']]],
+  ['rtc_5finit',['RTC_init',['../group__rtc.html#ga83823ddc481026aea712ce81c4621b4c',1,'RTC_init(void):&#160;rtc.c'],['../group__rtc.html#ga83823ddc481026aea712ce81c4621b4c',1,'RTC_init(void):&#160;rtc.c'],['../group__rtc.html#ga83823ddc481026aea712ce81c4621b4c',1,'RTC_init(void):&#160;rtc.c']]],
+  ['rtc_5fsettime',['RTC_setTime',['../group__rtc.html#ga70993ebbbb06a80b16b64f90c72cdf60',1,'RTC_setTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga70993ebbbb06a80b16b64f90c72cdf60',1,'RTC_setTime(uint32_t *time):&#160;rtc.c'],['../group__rtc.html#ga70993ebbbb06a80b16b64f90c72cdf60',1,'RTC_setTime(uint32_t *time):&#160;rtc.c']]]
 ];
