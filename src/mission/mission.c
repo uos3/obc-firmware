d4a685249debd7049091a80cb2babf6d720a4694
Shasaur <shasaur@hotmail.co.uk>
2018-09-08 22:04:57 +0100

    Added timer and interrupt functionality to OS for re-scheduling tasks

--- src/mission/mission.c
+++ src/mission/mission.c
@@ -9,7 +9,15 @@
 #include "../firmware.h"
 #include "mission.h"
 
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "inc/hw_memmap.h"
+
 #define UART_INTERFACE UART_GNSS
+
+#define TELEMETRY_SIZE 107 // 104 (tel) + 2 (timestamp) + 1 (id)
 
 opmode_t modes[8];
 
@@ -22,6 +30,40 @@ volatile uint8_t no_of_tasks_queued;
 // Properties
 task_t* current_tasks;
 uint8_t current_mode; // for deciding which enums to use
+
+uint8_t timer_to_task[6]; // timer_id -> task_id (reset look-up table)
+
+void timer0_isr(){
+	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+  UART_puts(UART_INTERFACE, "**timer0_interrupt!**\r\n");
+}
+
+void timer1_isr(){
+	TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+  UART_puts(UART_INTERFACE, "**timer1_interrupt!**\r\n");
+
+  // get task id and etc..
+
+}
+
+uint32_t ALL_TIMER_PERIPHERALS[6] =
+  { SYSCTL_PERIPH_TIMER0, SYSCTL_PERIPH_TIMER1, SYSCTL_PERIPH_TIMER2,
+    SYSCTL_PERIPH_TIMER3, SYSCTL_PERIPH_TIMER4, SYSCTL_PERIPH_TIMER5 };
+
+uint32_t ALL_TIMER_BASES[6] =
+  {TIMER0_BASE, TIMER1_BASE, TIMER2_BASE, TIMER3_BASE, TIMER4_BASE, TIMER5_BASE};
+
+void set_timer_for_task(uint8_t id, uint32_t period, void (*timer_isr)() ){
+  TimerLoadSet(ALL_TIMER_BASES[id], TIMER_A, period-1); // prime it (-1 is unnecessery but keep it just to avoid sysclock conflict?)
+
+  TimerIntRegister(ALL_TIMER_BASES[id], TIMER_A, timer_isr); // this is for dynamic interrupt compilation
+
+  TimerEnable(ALL_TIMER_BASES[id], TIMER_A); 	 // start timer
+}
+
+void stop_timer_for_task(uint8_t id){
+  TimerDisable(ALL_TIMER_BASES[id], TIMER_A);
+}
 
 void Mission_init(void)
 {
@@ -37,6 +79,40 @@ void Mission_init(void)
     task_q[i] = -1;
   }
   no_of_tasks_queued = 0;
+
+
+
+  for (uint8_t i=0; i<6; i++) SysCtlPeripheralEnable(ALL_TIMER_PERIPHERALS[i]);
+  for (uint8_t i=0; i<6; i++) while(!SysCtlPeripheralReady(ALL_TIMER_PERIPHERALS[i]));
+  for (uint8_t i=0; i<6; i++){
+    TimerConfigure(ALL_TIMER_BASES[i], TIMER_CFG_PERIODIC); // set it to periodically trigger interrupt
+    TimerIntEnable(ALL_TIMER_BASES[i], TIMER_TIMA_TIMEOUT); // enable interrupt
+  }
+
+  uint32_t ulPeriod = (SysCtlClockGet()) ; // 1Hz update rate
+   // third will miss sometimes if processor busy
+
+  // -- get available timer
+
+  set_timer_for_task(0, ulPeriod, &timer0_isr);
+  set_timer_for_task(1, ulPeriod*2, &timer1_isr);
+
+  Delay_ms(3500);
+
+  stop_timer_for_task(0);
+  stop_timer_for_task(1);
+
+  Delay_ms(3500);
+
+  set_timer_for_task(0, ulPeriod/2, &timer0_isr);
+  set_timer_for_task(1, ulPeriod, &timer1_isr);
+
+  Delay_ms(3500);
+
+  stop_timer_for_task(0);
+  stop_timer_for_task(1);
+
+  Delay_ms(3500);
 
   Mode_init(NF);
 }
@@ -62,6 +138,9 @@ void Mode_init(int8_t type){
       task_q[no_of_tasks_queued] = SAVE_EPS_HEALTH;
       no_of_tasks_queued++;
 
+      task_q[no_of_tasks_queued] = TRANSMIT;
+      no_of_tasks_queued++;
+
     } break;
   }
 }
@@ -82,14 +161,17 @@ void Mission_loop(void)
   if (!circ_isEmpty(&task_pq)){
     // peek
     uint8_t todo_task_index = circ_peek(&task_pq);
-    char output[100];
-    sprintf(output,"%+06d\r\n", todo_task_index);
-    UART_puts(UART_INTERFACE, output);
+    // char output[100];
+    // sprintf(output,"%+06d\r\n", todo_task_index);
+    // UART_puts(UART_INTERFACE, output);
 
     // execute
     UART_puts(UART_INTERFACE, "**executing**\r\n");
 
     current_tasks[todo_task_index].TickFct(NULL);
+
+    // reset timer for that tasks
+
 
     // pop
     circ_pop(&task_pq);
@@ -98,7 +180,7 @@ void Mission_loop(void)
   // Sleep
   UART_puts(UART_INTERFACE, "sleeping...\r\n");
   LED_toggle(LED_B);
-  Delay_ms(100);
+  Delay_ms(1000);
   WDT_kick();
 }
 
@@ -108,12 +190,66 @@ void Mission_SEU(void)
 
 }
 
+
+
 int8_t save_eps_health_data(int8_t t){
-  UART_puts(UART_INTERFACE, "[TASK] Saving EPS health data\r\n");
+  char telemetry[104]; //832 bits (104 bytes) available for each payload
+  uint8_t i; // loop counter
+
+  // Generate dataset id
+  telemetry[0] = (uint32_t) 0;
+
+  // Get timestamp
+  telemetry[2] = (uint16_t) 0;
+
+
+  uint8_t pos = 3; // current byte / 104 in payload
+
+  // Get TOBC values
+  for (i=0; i<10; i++)
+    telemetry[pos+i] = 0;
+  pos+=10;
+
+  // Get 45 EPS values
+  for (i=0; i<45; i++)
+    telemetry[pos+i] = 0;
+  pos+=45;
+
+  // Check subsystems and record status of 14
+  for (i=0; i<14; i++)
+    telemetry[pos+i] = 0;
+  pos+=14;
+
+  // Save telemetry to FRAM
+
+  // Insert new entry into FRAM lookup table (packet_id -> FRAM pos)
+  // ..do this by finding next available pos
+
+  UART_puts(UART_INTERFACE, "[TASK #001] Finished saving EPS health data.\r\n");
 }
 
 int8_t transmit_next_telemetry(int8_t t){
-  UART_puts(UART_INTERFACE, "[TASK] Transmit next telemetry\r\n");
+  uint8_t data_read[TELEMETRY_SIZE];
+
+  // Read lookup table to find where to look in FRAM
+
+
+  // Load packet from FRAM
+  // get next packet to read
+  // FRAM_read(0x00000, data_read, TELEMETRY_SIZE);
+
+  // add authentication hash
+
+  // add crc
+
+
+  // Transfer to TX transceiver
+
+
+  // Transmit
+
+
+  UART_puts(UART_INTERFACE, "[TASK #002] Finished transmit next telemetry\r\n");
 }
 
 // Utility
