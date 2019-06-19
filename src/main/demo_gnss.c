e3ffe60a8c70e10248388bdb34470b5109828585
Travis CI <travis@travis-ci.org>
2019-06-19 17:49:06 +0100

    tidy gnss demo

--- src/main/demo_gnss.c
+++ src/main/demo_gnss.c
@@ -7,30 +7,16 @@
 
 int main(void){
 
-    int32_t *longitude_p, *latitude_p, *altitude_p;
-    uint64_t *ex_time_p;
     uint64_t ex_time = 0;
-    uint32_t *seconds_p;
     uint32_t seconds = 0;
-    uint16_t *week_p;
     uint16_t week = 0;
     int32_t longitude=0, latitude=0, altitude=0;
-    uint8_t *long_sd_p, *lat_sd_p, *alt_sd_p;
     uint8_t long_sd=0, lat_sd=0, alt_sd=0;
 
     Board_init();
     WDT_kick();
     LED_on(LED_B);
 
-    longitude_p = &longitude;
-    latitude_p = &latitude;
-    altitude_p = &altitude;
-    long_sd_p = &long_sd;
-    lat_sd_p = &lat_sd;
-    alt_sd_p = &alt_sd;
-    ex_time_p = &ex_time;
-    week_p = &week;
-    seconds_p = &seconds;
     //To ensure the GNSS has time to boot up
     Delay_ms(5000);
     //IMPLEMENT RETURN 1 LOGIC -- ALL ZEROS
@@ -41,11 +27,11 @@ int main(void){
         UART_init(UART_CAMERA, 9600);
         UART_puts(UART_CAMERA, "\n\rGNSS data collection:\n\r");
         char message[500];
-        GNSS_getData(zzzlongitude_p, latitude_p, altitude_p, long_sd_p, lat_sd_p, alt_sd_p, week_p, seconds_p, ex_time_p);
+        GNSS_getData(&longitude, &latitude, &altitude, &long_sd, &lat_sd, &alt_sd, &week, &seconds, &ex_time);
         UART_puts(UART_CAMERA, "\n\rData collection complete:");
         sprintf(message, "\n\rLongitude: %" PRId32 "\n\rLatitude: %" PRId32 "\n\rAltitude: %" PRId32 "\n\rLatitude s_d: %" PRId8 
         "\n\rLongitude s_d: %" PRId8 "\n\rAltitude s_d: %" PRId8 "\n\rTime to execute: %" PRId64 " milliseconds \n\rWeek Number: %" PRId16 "\n\rWeek Seconds: %" PRId32 "\n\r",
-        *longitude_p, *latitude_p, *altitude_p, *long_sd_p, *lat_sd_p, *alt_sd_p, *ex_time_p, *week_p, *seconds_p);
+        longitude, latitude, altitude, long_sd, lat_sd, alt_sd, ex_time, week, seconds);
         UART_puts(UART_CAMERA, message);
         UART_puts(UART_CAMERA, "Waiting for next collection...\n\r");
         Delay_ms(2500);
