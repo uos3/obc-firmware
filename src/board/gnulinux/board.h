185fd0bc25e92fb497c3203d738a9bfd03246741
Maciej <md9u17@soton.ac.uk>
2019-07-08 16:32:23 +0100

    nn

--- src/board/gnulinux/board.h
+++ src/board/gnulinux/board.h
@@ -7,13 +7,13 @@
 #define LED_B	0x1
 
 
-#define UART_GNSS	0x0
+#define UART_GNSS	    0x0
 #define UART_CAMERA		0x1
 
 
 #define SPI_RADIO_RX	0x0
 #define SPI_RADIO_TX	0x1
-#define SPI_FRAM			0x2
+#define SPI_FRAM		0x2
 
 
 #define GPIO0_RADIO_RX	0x0
