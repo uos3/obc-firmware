2ea30d011a796c9b05d307131bba74420c1f6964
Maciej <md9u17@soton.ac.uk>
2019-07-18 14:47:03 +0100

    Antenna demo preparation

--- src/main/demo_antenna.c
+++ src/main/demo_antenna.c
@@ -1,33 +1,58 @@
 #include <stdio.h>
 #include "../firmware.h"
 
-#define UART_INTERFACE UART_GNSS
-#define BURN_TIME 6000
+#define BURN_TIME 7140
 #define WAIT_TIME 30000
 
 void Antenna_deploy_demo(void);
 
 int main(){
     Board_init();
+    RTC_init();  
     UART_init(UART_INTERFACE, 9600);
-    char output[30];
+    UART_puts(UART_INTERFACE, "\r\n");
+    int rows = 5;
+    for(int i = rows; i >= 1; --i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            UART_puts(UART_INTERFACE,"* ");
+        }
+        UART_puts(UART_INTERFACE, "\r\n");
+    }
+    UART_puts(UART_INTERFACE, "Board and UART Interface Initialised\r\n");
+    char output[50];
+    uint64_t timestamp_before, timestamp_after;
     Delay_ms(500);
-    UART_puts(UART_INTERFACE, "\r\n**Antenna deployment demo**\r\n");
+    UART_puts(UART_INTERFACE, "\r\nADM test started... Waiting 30 seconds...\n");
+    UART_puts(UART_INTERFACE, "\r\nLED_A will turn on when resistor is initiated, then off when the test is over\r\n");
 
-
-    Delay_ms(WAIT_TIME);
-
-    LED_on(LED_B);
-
-    sprintf(output, "Burn of %d milliseconds has begun\r\n", BURN_TIME);
+    for(uint16_t i=0; i<WAIT_TIME/1000; i++)
+    {
+      sprintf(output, "%d\t", WAIT_TIME/1000 - i);
+      UART_puts(UART_INTERFACE, output);
+      Delay_ms(1000);
+      if(i%5==4) UART_puts(UART_INTERFACE, "\r\n");
+    }
+    //Delay_ms(WAIT_TIME);
+    LED_on(LED_A);
+    sprintf(output, "\r\nBurn of %d milliseconds has begun\r\n", BURN_TIME);
+    UART_puts(UART_INTERFACE, output);
+    RTC_getTime_ms(&timestamp_before);
+    sprintf(output, "\r\nSystem time before burning is %lu ms", timestamp_before);
     UART_puts(UART_INTERFACE, output);
 
-    Antenna_deploy_demo();
+    Antenna_deploy_demo();      //call deploy function
 
     LED_off(LED_B);
+    UART_puts(UART_INTERFACE, "\r\nBurn has finished\r\n");
+    RTC_getTime_ms(&timestamp_after);
+    sprintf(output, "\r\nSystem time after burning is %lu ms\r\n", timestamp_after);
+    UART_puts(UART_INTERFACE, output);
 
-    UART_puts(UART_INTERFACE, "Burn has finished\r\n");
-
+    uint64_t difference = timestamp_after - timestamp_before;
+    sprintf(output, "\r\nThe burning time was %lu ms total\r\n", difference);
+    UART_puts(UART_INTERFACE, output);
     return 0;
 }
 
