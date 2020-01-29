792d9f3ad1486617771a4482a385a9b67935c560
Richard A <dracod500@gmail.com>
2020-01-29 14:41:48 +0000

    moved component files

--- mv_files.sh
+++ mv_files.sh
@@ -3,7 +3,7 @@
 source_file=$1
 source_dir="src/board/"
 source_dir_c="src/board/uos3-proto/"
-dest_dir="src/driver/"
+dest_dir="src/component/"
 
 mv $source_dir$source_file.h $dest_dir$source_file.h
 mv $source_dir_c$source_file.c $dest_dir$source_file.c
