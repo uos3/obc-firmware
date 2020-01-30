79bb8eace05abaa4ffc292433796a30aa9468a94
Richard A <dracod500@gmail.com>
2020-01-30 16:18:24 +0000

    updated compilation process

--- src/firmware.mk
+++
@@ -1,7 +0,0 @@
-FW_SRCS=$(FW_ROOT)/src/$(MAIN).c       \
-    $(wildcard $(FW_ROOT)/src/driver/*.c)   \
-    $(wildcard $(FW_ROOT)/src/component/*.c) \
-    $(wildcard $(FW_ROOT)/src/utility/*.c) \
-    $(wildcard $(FW_ROOT)/src/mission/*.c)
-
-UDEFS += $(DEFINES)
