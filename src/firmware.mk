3c0db83bbbe81259f91635f3e67b25dbd509ea42
Richard A <dracod500@gmail.com>
2020-01-30 11:31:15 +0000

    new file structure compiles

--- src/firmware.mk
+++ src/firmware.mk
@@ -1,9 +1,7 @@
 FW_SRCS=$(FW_ROOT)/src/$(MAIN).c       \
-    $(wildcard $(FW_ROOT)/src/cc112x/*.c)   \
-    $(wildcard $(FW_ROOT)/src/buffer/*.c)   \
-    $(wildcard $(FW_ROOT)/src/packet/*.c)   \
-    $(wildcard $(FW_ROOT)/src/utility/*.c)   \
-    $(wildcard $(FW_ROOT)/src/configuration/*.c) \
+    $(wildcard $(FW_ROOT)/src/driver/*.c)   \
+    $(wildcard $(FW_ROOT)/src/component/*.c) \
+    $(wildcard $(FW_ROOT)/src/utility/*.c) \
     $(wildcard $(FW_ROOT)/src/mission/*.c)
 
 UDEFS += $(DEFINES)
