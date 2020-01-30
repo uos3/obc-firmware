94779aa8667bc3641f4ba2da6f9fbde0d5cc92ee
Richard A <dracod500@gmail.com>
2020-01-30 16:18:38 +0000

    removed supporting script

--- mv_files.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-source_file=$1
-source_dir="src/board/"
-source_dir_c="src/board/uos3-proto/"
-dest_dir="src/component/"
-
-mv $source_dir$source_file.h $dest_dir$source_file.h
-mv $source_dir_c$source_file.c $dest_dir$source_file.c
