baa741f6c999e54f4428cc8bd38e7c3e8e330676
Yizhe Koh <yz@su-shop-pos06.soton.ac.uk>
2020-02-13 18:11:08 +0000

    Demo camera dirty new

--- src/main/demo_camera_dirty_new.c
+++ src/main/demo_camera_dirty_new.c
@@ -11,16 +11,18 @@
 #include "../driver/uart.h"
 #include "../driver/rtc.h"
 #include "../driver/delay.h"
+#include "../utility/debug.h"
 
 #include "../component/led.h"
 #include "../component/fram.h"
+#include "../driver/wdt.h"
+#include "../driver/watchdog_ext.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 //#define PAGE_LENGTH 5000 //Buffer_slot_size is 3392bytes, do changed the page size!
 #define PAGE_LENGTH 106
-#define PAGE_SIZE 2080
 
 static char LK_RESET[]     = {0x56, 0x00, 0x26, 0x00};
 static char LK_RESET_RE[]    = {0x0d, 0x0a, 0x49, 0x6e, 0x69, 0x74, 0x20, 0x65, 0x6e, 0x64, 0x0d, 0x0a};
@@ -68,6 +70,7 @@ int main(void)
     watchdog_update = 0xFF;
     int index_rst=0, index_res=0, index_takepic=0, index_readsize=0,index_readdata=0, index_readdata2 =0, index_resolution = 0;;
     int rst = 0, res = 0, pic = 0, size=0, dat =0, dat2 = 0, dat3=0, resol=0;
+    int PAGE_SIZE = 2080
     char reset_resp[20], res_resp[20]={0}, pic_res[20], size_resp[20]={0}, read_resp[20]={0}, read_resp2[20], resol_resp[20]={0};
     char *output = malloc(100*sizeof(char));
     char *output2 = malloc(100*sizeof(char));
