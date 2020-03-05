b0f609c0e242b929fc0c9fb353b942d54bb0313b
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-05 21:37:06 +0000

    authentication layer support for new base

--- src/mission/packet_auth.h
+++ src/mission/packet_auth.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include "packet_base.h"
 
+typedef struct packet_hash_t{
+	uint8_t bytes[16];
+}packet_hash_t;
+
 void packet_hasher(Packet* packet);
 
 uint8_t packet_is_authentic(Packet* packet);
