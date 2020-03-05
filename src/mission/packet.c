2f589cb3c458fd0543de62e961d85d44b95457df
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-05 21:51:27 +0000

    high level packet interface

--- src/mission/packet.c
+++ src/mission/packet.c
@@ -4,10 +4,47 @@
 #include "packet_application.h"
 #include "buffer.h"
 
-void packet_store_data(uint8_t whofor, uint8_t is_unfinished, uint8_t data, uint32_t data_len){
+
+void packet_prep_buffer(){
+	uint8_t current_block_length;
+	uint8_t block_remaining_length;
+
+	// the total amount of bytes in the block, including reserved
+	current_block_length = buffer_status.current_block_position;
+	// the total writable length. Remember, first byte in block is the length/status byte.
+	block_remaining_length = BLOCK_SIZE - current_block_length;
+
+	/*
+		if we can't fit the header in the packet, there's no point writing.
+		we then move to the next block. No partial headers, please.
+	*/
+	if (block_remaining_length <= APPLICATION_HEADER_LEN){
+		buffer_increment_block();
+	}
+}
+
+
+uint64_t packet_is_unfinished(data_len){
+	if (data_len > buffer_status.current_block_position){
+		return 0xFF;
+	}
+	return 0x00;
+}
+
+
+void store_payload_data(uint8_t whofor, uint8_t data, uint32_t data_len){
 	app_header_t header;
-	app_info_t info;
-	header = app_header_constructor();
-	info = app_info_fromfields(whofor, is_unfinished, 0, 0, 0);
-	*header.info_start = app_info_asbyte(info);
+	uint8_t is_unfinished;
+
+	// first, prep the buffer to prevent incomplete headers
+	packet_prep_buffer();
+
+	// build the header
+	header.as_struct.length = data_len;
+	is_unfinished = packet_is_unfinished(data_len);
+	header.as_struct.info = app_info_fromfields(whofor, is_unfinished, 0, 0, 0);
+
+	// write to buffer
+	buffer_write_next(header.as_bytes, APPLICATION_HEADER_LEN);
+	buffer_write_next(data, data_len);
 }
---
+++ src/mission/packet.h
@@ -0,0 +1,16 @@
+#include "packet_base.h"
+#include "packet_auth.h"
+#include "packet_transport.h"
+#include "packet_application.h"
+
+/*
+	should mirror each blocks structure in the buffer.
+	As much as I'd like to do clever things with some weird type where you can
+	have arbitary start points for non-standard length members, this is easier
+*/
+typedef struct typed_packet_t{
+	uint8_t length;
+	packet_hash_t hash;
+	transport_header_t transport_header;
+	uint8_t data_bytes[BLOCK_SIZE-BUFFER_DATA_START_INDEX];
+}typed_packet_t;
