1c0f264146fde026dcbb1203d4772e49af040c3a
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-06 11:13:22 +0000

    added endian-ness fixer

--- src/mission/packet/packet_transport.h
+++ src/mission/packet/packet_transport.h
@@ -9,6 +9,8 @@
 
 #define PACKET_TYPE_ACK 0xF
 // can use bit field addresses to construct the transport information section
+#pragma pack(1)
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 typedef struct transport_info_t
 {
 	int packet_type : 4;
@@ -17,12 +19,27 @@ typedef struct transport_info_t
 	int initial_packet : 1;
 	int do_not_continue : 1;
 } transport_info_t;
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+typedef struct transport_info_t
+{
+	int do_not_continue : 1;
+	int initial_packet : 1;
+	int end_of_sequence : 1;
+	int start_of_sequence : 1;
+	int packet_type : 4;
+} transport_info_t;
+#endif
 
-
-typedef struct transport_header_t{
+typedef struct _transport_header_struct_t{
 	PACKET_SEQUENCE_TYPE sequence_number;
 	transport_info_t info;
+}_transport_header_struct_t;
+
+typedef union transport_header_t{
+	_transport_header_struct_t as_struct;
+	uint8_t as_bytes[sizeof(_transport_header_struct_t)];
 }transport_header_t;
+#pragma pack()
 
 
 uint8_t transport_info_asbyte(transport_info_t transport_info);
