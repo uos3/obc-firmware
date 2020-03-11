70d6bd847956f55bf7efcf8a1343bb5e05642972
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-11 13:31:05 +0000

    1-time print of the dummy fram start address

--- src/linux/fram.c
+++ src/linux/fram.c
@@ -5,12 +5,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "../driver/fram.h"
+#include "../utility/debug.h"
 
-static uint8_t dummy_fram[FRAM_SIZE];
+uint8_t dummy_fram[FRAM_SIZE];
 
-bool FRAM_selfTest(void);
+bool FRAM_selfTest(void){
+
+}
 
 void FRAM_write(uint32_t address, uint8_t *data, uint32_t length){
+	static bool have_written;
+	if (!have_written){
+		debug_printf("FRAM.C: dummy_fram start address: %p", &dummy_fram);
+		have_written = true;
+	}
+	
+	// debug_printf("FRAM.C: writing to FRAM address: ")
 	for(uint32_t i = 0; i<length; i++){
 		dummy_fram[i+address] = data[i];
 	}
