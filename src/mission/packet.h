68dc0ba5d35ad11cc2c7f0378963ad6a56286afb
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-11 13:32:02 +0000

    added endian check

--- src/mission/packet.h
+++ src/mission/packet.h
@@ -10,12 +10,22 @@
 	have arbitary start points for non-standard length members, this is easier
 */
 #pragma pack(1)
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 typedef struct packet_typed_struct{
-	uint8_t data_bytes[BLOCK_SIZE-BUFFER_DATA_START_INDEX];
-	transport_header_t transport_header;
+	// uint8_t data_bytes[BLOCK_SIZE-BUFFER_DATA_START_INDEX];
+	transport_header_struct transport_header;
 	packet_hash_t hash;
 	uint8_t length;
 }packet_typed_struct;
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+typedef struct packet_typed_struct{
+	uint8_t length;
+	packet_hash_t hash;
+	transport_header_struct transport_header;
+	// uint8_t data_bytes[BLOCK_SIZE-BUFFER_DATA_START_INDEX];
+}packet_typed_struct;
+#endif
 
 typedef union packet_typed_t{
 	packet_typed_struct as_struct;
