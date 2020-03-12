361ca881118d404a0f83593737dd98c2e4684331
Richard A <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-03-12 15:31:27 +0000

    updated to bring to SWS compliance

--- src/mission/packet_application.c
+++ src/mission/packet_application.c
@@ -1,40 +1,49 @@
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "packet_base.h"
 #include "packet_application.h"
 #include "buffer.h"
 #include "../utility/byte_plexing.h"
 
-uint8_t app_info_asbyte(app_info_t app_info){
-	uint8_t byte;
-	byte = 0;
-	byte |= ((uint8_t) app_info.whofor & 0x0F) << 4;
-	byte |= ((uint8_t) app_info.unfinished & 0x01) << 3;
-	byte |= ((uint8_t) app_info.ret &0x01) << 2;
-	byte |= ((uint8_t) app_info.confirm &0x01) << 1;
-	byte |= ((uint8_t) app_info.now &0x01);
-	return byte;
-}
 
-app_info_t app_info_frombyte(uint8_t byte){
-	app_info_t app_info;
-	app_info.whofor 	= ((byte & 0xF0) >> 4); // mask with 11110000 and shift right 4 times
-	app_info.unfinished = ((byte & 0x08) >> 3); // 00001000
-	app_info.ret 		= ((byte & 0x04) >> 2); // 00000100
-	app_info.confirm 	= ((byte & 0x02) >> 1); // 00000010
-	app_info.now 		=  (byte & 0x01);		// 00000001
-	return app_info;
-}
-
-app_info_t app_info_fromfields(uint8_t whofor, uint8_t is_unfinished, uint8_t is_ret, uint8_t is_confirm, uint8_t is_now){
+app_info_t app_info_fromfields(uint8_t whofor, bool is_unfinished, bool is_ret, bool is_confirm, bool is_now){
 	app_info_t app_info;
 	app_info.whofor = (whofor & 0x0F);		 // mask with 11110000 and shift right 4 times
-	app_info.unfinished = ((is_unfinished & 0x08) >> 3); // 00001000
-	app_info.ret = ((is_ret & 0x04) >> 2);   // 00000100
-	app_info.confirm = ((is_confirm & 0x02) >> 1);	// 00000010
-	app_info.now = (is_now & 0x01); // 00000001
+	app_info.unfinished = is_unfinished;
+	app_info.ret = is_ret;
+	app_info.confirm = is_confirm;
+	app_info.now = is_now;
 	return app_info;
+}
+
+app_header_t app_header_fromfields(APPLICATION_LEN_TYPE data_length, uint8_t whofor, bool is_unfinished, bool is_ret, bool is_confirm, bool is_now){
+	app_info_t info;
+	app_header_t header;
+	APPLICATION_LEN_TYPE correct_endian_length;
+	correct_endian_length = data_length;
+	info = app_info_fromfields(whofor, is_unfinished, is_ret, is_confirm, is_now);
+
+	#if little_endian
+		flip_endian(correct_endian_length, APPLICATION_LEN_LEN);
+	#endif
+
+	header.as_struct.info = info;
+	header.as_struct.length = correct_endian_length;
+	return header;
+}
+
+app_header_t app_header_frombytes(uint8_t* data){
+	app_header_t header;
+	
+	memcpy(header.as_bytes, data, APPLICATION_HEADER_LEN);
+
+	#if little_endian
+		flip_endian(header.as_struct.length, APPLICATION_LEN_LEN);
+	#endif
+
+	return header;
 }
 
 bool app_retrieve_header(app_header_t* header, uint8_t* data_array, uint8_t data_array_length, uint8_t header_start_index){
@@ -50,7 +59,7 @@ bool app_retrieve_header(app_header_t* header, uint8_t* data_array, uint8_t data
 }
 
 bool app_is_unfinished(uint32_t data_len){
-	if (data_len > buffer_get_free_length()){
+	if (data_len > (BLOCK_SIZE - BUFFER_DATA_START_INDEX)){
 		return true;
 	}
 	return false;
