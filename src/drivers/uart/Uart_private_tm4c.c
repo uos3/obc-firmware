e71629b476cd076de1db7f9f3ed65daab9c28fff
LeoGalanakis <leongalanakis@gmail.com>
2021-02-22 01:28:41 +0000

    Added prototype UART functions, defined UART device structs.

--- src/drivers/uart/Uart_private_tm4c.c
+++ src/drivers/uart/Uart_private_tm4c.c
@@ -28,9 +28,60 @@
 #include "drivers/uart/Uart_private.h"
 #include "drivers/uart/Uart_errors.h"
 
+/* TI Library Includes */
+#include "driverlib/gpio.h"
+#include "driverlib/uart.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+
 /* -------------------------------------------------------------------------   
  * GLOBALS
  * ------------------------------------------------------------------------- */
+
+Uart_Device UART_PINS[UART_NUM_UARTS] = {
+    {
+        /* UART 1 (GNSS) */
+        SYSCTL_PERIPH_GPIOA,
+        SYSCTL_PERIPH_UART0,
+        GPIO_PORTA_BASE,
+        UART0_BASE,
+        GPIO_PA0_U0RX, /* TODO: Getting same error as I2C. Should still work */
+        GPIO_PA1_U0TX,
+        GPIO_PIN_0,
+        GPIO_PIN_1,
+        false
+    },
+
+    {
+        /* UART 2 (CAM) */
+        SYSCTL_PERIPH_GPIOC,
+        SYSCTL_PERIPH_UART3,
+        GPIO_PORTC_BASE,
+        UART3_BASE,
+        GPIO_PC6_U3RX, /* TODO: Getting same error as I2C. Should still work */
+        GPIO_PC7_U3TX,
+        GPIO_PIN_6,
+        GPIO_PIN_7,
+        false
+    },
+
+    {
+        /* UART 3 (PWR) */
+        SYSCTL_PERIPH_GPIOE,
+        SYSCTL_PERIPH_UART7,
+        GPIO_PORTE_BASE,
+        UART7_BASE,
+        GPIO_PE0_U7RX, /* TODO: Getting same error as I2C. Should still work */
+        GPIO_PE1_U7TX,
+        GPIO_PIN_0,
+        GPIO_PIN_1,
+        false
+    },
+
+    /* TODO: Add debugging UARTs */
+};
 
 /* -------------------------------------------------------------------------   
  * FUNCTIONS
--- src/drivers/uart/Uart_public.h
+++ src/drivers/uart/Uart_public.h
@@ -25,10 +25,7 @@
 #include <string.h>
 
 /* Internal */
-#include "drivers/uart/Uart_private.h"
 #include "drivers/uart/Uart_errors.h"
-
-#include "drivers/uart/Uart_private_tm4c.c"
 
 /* -------------------------------------------------------------------------   
  * GLOBALS
@@ -41,9 +38,90 @@
 /* -------------------------------------------------------------------------   
  * STRUCTS
  * ------------------------------------------------------------------------- */
+/**
+ * @brief Defines a UART.
+ * 
+ */
+typedef struct _Uart_Device {
+    uint32_t gpio_peripheral;
+    uint32_t uart_peripheral;
+    uint32_t gpio_base;
+    uint32_t uart_base;
+    uint32_t uart_pin_rx_func;
+    uint32_t uart_pin_tx_func;
+    uint8_t gpio_pin_rx;
+    uint8_t gpio_pin_tx;
+    bool initialised;
+} Uart_Device;
 
 /* -------------------------------------------------------------------------   
  * FUNCTIONS
  * ------------------------------------------------------------------------- */
+
+/**
+ * @brief Initialises the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_init(uint8_t uart_id_number_in);
+
+/**
+ * @brief Receieves a byte from the RX FIFO of the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @param is_blocking_in bool to check if the function should be nonblocking.
+ * @param recvd_byte_out Byte received from the UART peripheral.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_get_char(uint8_t uart_id_number_in, bool is_blocking_in, char *recvd_byte_out);
+
+/**
+ * @brief Sends a byte from the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @param is_blocking_in bool to check if the function should be nonblocking.
+ * @param byte_out Byte to send.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_put_char(uint8_t uart_id_number_in, bool is_blocking_in, char byte_out);
+
+/**
+ * @brief Sends a string (/0 terminated) from the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @param is_blocking_in bool to check if the function should be nonblocking.
+ * @param string_out Pointer to the string to send.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_put_string(uint8_t uart_id_number_in, bool is_blocking_in, char *string_out);
+
+/**
+ * @brief Sends a buffer with a specified length from the specified UART
+ *  peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @param is_blocking_in bool to check if the function should be nonblocking.
+ * @param buffer_length_in Length of buffer to send (in bytes).
+ * @param buffer_out Pointer to the buffer/string to send.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_put_buffer(uint8_t uart_id_number_in, bool is_blocking_in, size_t buffer_length_in, char *buffer_out);
+
+/**
+ * @brief Checks the TX FIFO busy status of the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_busy_check_tx(uint8_t uart_id_number_in);
+
+/**
+ * @brief Checks the RX FIFO busy status of the specified UART peripheral.
+ * 
+ * @param uart_id_number_in UART peripheral ID number.
+ * @return ErrorCode 
+ */
+ErrorCode Uart_busy_check_rx(uint8_t uart_id_number_in);
 
 #endif /* H_UART_PUBLIC_H */
