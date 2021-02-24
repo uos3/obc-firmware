24f02020c0dad387486ab1c59b07768c29b25cdc
Duncan Hamill <duncanrhamill@googlemail.com>
2021-02-24 14:11:36 +0000

    UART interface setup

--- src/drivers/uart/Uart_public.h
+++ src/drivers/uart/Uart_public.h
@@ -114,4 +114,75 @@ ErrorCode Uart_busy_check_tx(uint8_t uart_id_number_in);
  */
 ErrorCode Uart_busy_check_rx(uint8_t uart_id_number_in);
 
+/* -------------------------------------------------------------------------   
+ * INTERFACE 2.0
+ * ------------------------------------------------------------------------- */
+
+typedef uint8_t Uart_DeviceId;
+
+#define UART_DEVICE_ID_GNSS (0)
+
+/**
+ * @brief Initialises the all used UART peripherals.
+ * 
+ * If not all UARTs are initialised correctly an error is returned for
+ * monitoring purposes only. If all UARTs fail to initialise an error is also
+ * returned, but indicates failure of the UART system itself.
+ *
+ * @return ErrorCode If no error, ERROR_NONE, otherwise UART_ERROR_x.
+ */
+// ErrorCode Uart_init(void);
+
+/**
+ * @brief Initialise (or re-init) a specific UART device.
+ * 
+ * @param uart_id_in The device to re-initialise.
+ * @return ErrorCode TODO
+ */
+ErrorCode Uart_init_specific(Uart_DeviceId uart_id_in);
+
+/**
+ * @brief IF STATE MACHINE
+ * 
+ * @return ErrorCode 
+ */
+ErrorCode Uart_step(void);
+
+/**
+ * @brief Send bytes
+ * 
+ * Event EVT_UART_SEND_COMPLETE is raised when the bytes have been sent to the
+ * device. This could either be successful or it could fail, so use
+ * Uart_get_status() for the device to check.
+ * 
+ * @param uart_id_in 
+ * @param p_data_in 
+ * @param length_in 
+ * @return ErrorCode 
+ */
+ErrorCode Uart_send_bytes(
+    Uart_DeviceId uart_id_in,
+    uint8_t *p_data_in, 
+    size_t length_in
+);
+
+/**
+ * @brief Did it work?
+ * 
+ * @param uart_id_in 
+ * @param p_status_out 
+ * @return ErrorCode 
+ */
+ErrorCode Uart_get_status(
+    Uart_DeviceId uart_id_in,
+    void *p_status_out
+);
+
+
+ErrorCode Uart_recv_bytes(
+    Uart_DeviceId uart_id_in,
+    uint8_t *p_data_out,
+    size_t length_in
+);
+
 #endif /* H_UART_PUBLIC_H */
