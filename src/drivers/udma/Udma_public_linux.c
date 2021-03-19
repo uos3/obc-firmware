61618c1eccdb638d2b3b62770bb6d27f2ad9f3d6
LeoGalanakis <leongalanakis@gmail.com>
2021-03-19 18:29:49 +0000

    Fixed build errors for GPIO and UART tests. Updated respective CMake files

--- src/drivers/udma/Udma_public_linux.c
+++ src/drivers/udma/Udma_public_linux.c
@@ -1,11 +1,11 @@
 /**
  * @ingroup uart
  * 
- * @file Uart_public_linux.c
+ * @file Uart_public_tm4c.c
  * @author Leon Galanakis (lg5g16@soton.ac.uk)
  * @brief Public UART driver for the firmware.
  * 
- * Implements the UART driver for Linux.
+ * Implements the UART driver for the TM4C.
  * 
  * @version 0.1
  * @date 2021-02-16
@@ -27,45 +27,28 @@
 #include "drivers/uart/Uart_public.h"
 #include "drivers/uart/Uart_private.h"
 #include "drivers/uart/Uart_errors.h"
+#include "drivers/udma/Udma_errors.h"
+#include "drivers/udma/Udma_public.h"
 
 #include "util/debug/Debug_public.h"
+
+/* -------------------------------------------------------------------------   
+ * GLOBALS
+ * ------------------------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------   
  * FUNCTIONS
  * ------------------------------------------------------------------------- */
 
-ErrorCode Uart_init(void) {
-    DEBUG_DBG("UART init attempted when running on linux, ignoring.");
+ErrorCode Udma_init(void) {
+    DEBUG_DBG("uDMA init attempted when running on linux, ignoring.");
     return ERROR_NONE;
 }
 
-ErrorCode Uart_init_specific(Uart_DeviceId uart_id_in) {
-    DEBUG_DBG("UART init specific attempted when running on linux, ignoring.");
-    return ERROR_NONE;
-}
-
-ErrorCode Uart_send_bytes(
+ErrorCode Udma_interrupt_handler(
     Uart_DeviceId uart_id_in,
-    uint8_t *p_data_in, 
     size_t length_in
 ) {
-    DEBUG_DBG("UART send bytes attempted when running on linux, ignoring.");
-    return ERROR_NONE;
-}
-
-ErrorCode Uart_recv_bytes(
-    Uart_DeviceId uart_id_in,
-    uint8_t *p_data_out,
-    size_t length_in
-) {
-    DEBUG_DBG("UART recv bytes attempted when running on linux, ignoring.");
-    return ERROR_NONE;
-}
-
-ErrorCode Uart_get_status(
-    Uart_DeviceId uart_id_in,
-    uint8_t *p_status_out
-) {
-    DEBUG_DBG("UART get status attempted when running on linux, ignoring.");
+    DEBUG_DBG("uDMA int handler attempted when running on linux, ignoring.");
     return ERROR_NONE;
 }
