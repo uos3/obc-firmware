7838d973809deb15175bc10c75dfc4d70a65d5da
Duncan Hamill <duncanrhamill@googlemail.com>
2021-03-30 15:04:42 +0100

    Changed format spec for delay print in test_delay

--- src/test/test_delay.c
+++ src/test/test_delay.c
@@ -77,7 +77,7 @@ int main(void) {
         (double)expected_delay_ms;
 
     DEBUG_INF(
-        "Delay complete, expected %d ms delay, recorded %ld ms delay",
+        "Delay complete, expected %u ms delay, recorded %llu ms delay",
         expected_delay_ms,
         actual_delay_ms  
     );
