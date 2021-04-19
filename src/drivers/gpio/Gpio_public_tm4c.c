a5b0b48121ffa1c5e409f7b30cc781bc6bc41785
LeoGalanakis <leongalanakis@gmail.com>
2021-04-19 12:20:21 +0100

    Debugging for gpio test

--- src/drivers/gpio/Gpio_public_tm4c.c
+++ src/drivers/gpio/Gpio_public_tm4c.c
@@ -122,6 +122,8 @@ ErrorCode Gpio_init(GPIO_PIN_INDEX *p_gpio_pins_in, size_t num_gpio_pins_in, Gpi
 
     }
 
+    Debug_exit(1);
+
     /* Set the GPIO driver state as initialised */
     /* GPIO.initialised = true; */
     /* If this point has reached without any errors, return ERROR_NONE */
