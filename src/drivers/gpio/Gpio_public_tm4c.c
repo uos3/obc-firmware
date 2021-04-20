2d6fa4b25de3c0c57d53c76d3e0b7f66459d713a
LeoGalanakis <leongalanakis@gmail.com>
2021-04-20 14:45:26 +0100

    Removed some ampersands to fix some address issues

--- src/drivers/gpio/Gpio_public_tm4c.c
+++ src/drivers/gpio/Gpio_public_tm4c.c
@@ -297,22 +297,22 @@ ErrorCode Gpio_set_rising_interrupt(GPIO_PIN_INDEX gpio_id_number, void *interru
     /* Check the port and call the appropriate function */
     switch (p_gpio_pin->port) {
         case GPIO_PORTA_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_a_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_a_int_handler);
             break;
         case GPIO_PORTB_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_b_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_b_int_handler);
             break;
         case GPIO_PORTC_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_c_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_c_int_handler);
             break;
         case GPIO_PORTD_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_d_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_d_int_handler);
             break;
         case GPIO_PORTE_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_e_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_e_int_handler);
             break;
         case GPIO_PORTF_BASE:
-            GPIOIntRegister(&p_gpio_pin->port, Gpio_port_f_int_handler);
+            GPIOIntRegister(p_gpio_pin->port, Gpio_port_f_int_handler);
             break;
         default:
             DEBUG_ERR("Unexpected GPIO port");
