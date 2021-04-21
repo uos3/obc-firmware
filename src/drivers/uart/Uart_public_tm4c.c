a5e637e40013732687bf6141a170d57d2407e56e
LeoGalanakis <leongalanakis@gmail.com>
2021-04-21 15:31:38 +0100

    Fixed int registers and added udma int register

--- src/drivers/uart/Uart_public_tm4c.c
+++ src/drivers/uart/Uart_public_tm4c.c
@@ -183,6 +183,32 @@ ErrorCode Uart_send_bytes(
         return UDMA_ERROR_NOT_INITIALISED;
     }
 
+    /* Enable the UART interrupt.
+     * TODO: Check this, and in rx */
+    IntEnable(p_uart_device->uart_base_int);
+    UARTIntEnable(p_uart_device->uart_base, UART_INT_TX | UART_INT_DMATX);
+
+    switch(uart_id_in) {
+        case UART_DEVICE_ID_CAM:
+            UARTIntRegister(p_uart_device->uart_base, Uart_cam_tx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_cam_rx_int_handler);
+            break;
+        case UART_DEVICE_ID_GNSS:
+            UARTIntRegister(p_uart_device->uart_base, Uart_gnss_tx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_gnss_tx_int_handler);
+            break;
+        case UART_DEVICE_ID_EPS:
+            UARTIntRegister(p_uart_device->uart_base, Uart_eps_tx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_eps_tx_int_handler);
+            break;
+        case UART_DEVICE_ID_TEST:
+            UARTIntRegister(p_uart_device->uart_base, Uart_test_tx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_test_tx_int_handler);
+            break;
+    }
+
+    uDMAIntRegister(p_uart_device->udma_channel_tx, uDMAErrorHandler);
+    
     /* Configure the control parameters for the UART TX channel.
      * UDMA_ARB_4 to match the FIFO trigger threshold.
      * UDMA_ARB_4 refers to the arbitratoin size, which is the number
@@ -208,25 +234,6 @@ ErrorCode Uart_send_bytes(
 
     p_uart_device->uart_status_tx = UART_STATUS_IN_PROGRESS;
 
-    /* Enable the UART interrupt.
-     * TODO: Check this, and in rx */
-    IntEnable(p_uart_device->uart_base_int);
-    UARTIntEnable(p_uart_device->uart_base, UART_INT_TX | UART_INT_DMATX);
-
-    switch(uart_id_in) {
-        case UART_DEVICE_ID_CAM:
-            UARTIntRegister(p_uart_device->uart_base, Uart_cam_tx_int_handler);
-            break;
-        case UART_DEVICE_ID_GNSS:
-            UARTIntRegister(p_uart_device->uart_base, Uart_gnss_tx_int_handler);
-            break;
-        case UART_DEVICE_ID_EPS:
-            UARTIntRegister(p_uart_device->uart_base, Uart_eps_tx_int_handler);
-            break;
-        case UART_DEVICE_ID_TEST:
-            UARTIntRegister(p_uart_device->uart_base, Uart_test_tx_int_handler);
-            break;
-    }
 
     /* FIXME: uDMAErrorStatusGet */
 
@@ -266,6 +273,29 @@ ErrorCode Uart_recv_bytes(
         return UDMA_ERROR_NOT_INITIALISED;
     }
 
+    IntEnable(p_uart_device->uart_base_int);
+    UARTIntEnable(p_uart_device->uart_base, UART_INT_TX | UART_INT_DMATX);
+
+    switch(uart_id_in) {
+        case UART_DEVICE_ID_CAM:
+            UARTIntRegister(p_uart_device->uart_base, Uart_cam_rx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_cam_rx_int_handler);
+            break;
+        case UART_DEVICE_ID_GNSS:
+            UARTIntRegister(p_uart_device->uart_base, Uart_gnss_rx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_gnss_rx_int_handler);
+            break;
+        case UART_DEVICE_ID_EPS:
+            UARTIntRegister(p_uart_device->uart_base, Uart_eps_rx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_eps_rx_int_handler);
+            break;
+        case UART_DEVICE_ID_TEST:
+            UARTIntRegister(p_uart_device->uart_base, Uart_test_rx_int_handler);
+            IntRegister(p_uart_device->uart_base_int, Uart_test_rx_int_handler);
+            break;
+    }
+
+    uDMAIntRegister(p_uart_device->udma_channel_tx, uDMAErrorHandler);
 
     /* Configure the control parameters for the UART RX channel.
      * UDMA_ARB_4 to match the FIFO trigger threshold. 
@@ -298,23 +328,6 @@ ErrorCode Uart_recv_bytes(
     UARTIntEnable(p_uart_device->uart_base, UART_INT_RX | UART_INT_DMARX);
     #endif
     
-    IntEnable(p_uart_device->uart_base_int);
-    UARTIntEnable(p_uart_device->uart_base, UART_INT_TX | UART_INT_DMATX);
-
-    switch(uart_id_in) {
-        case UART_DEVICE_ID_CAM:
-            UARTIntRegister(p_uart_device->uart_base, Uart_cam_rx_int_handler);
-            break;
-        case UART_DEVICE_ID_GNSS:
-            UARTIntRegister(p_uart_device->uart_base, Uart_gnss_rx_int_handler);
-            break;
-        case UART_DEVICE_ID_EPS:
-            UARTIntRegister(p_uart_device->uart_base, Uart_eps_rx_int_handler);
-            break;
-        case UART_DEVICE_ID_TEST:
-            UARTIntRegister(p_uart_device->uart_base, Uart_test_rx_int_handler);
-            break;
-    }
 
     /* FIXME: Check uDMAErrorStatusGet */
 
