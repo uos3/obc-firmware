e6c013e967e887fd386c99c46ff1272205471a2c
kCbg <gianborgo@hotmail.com>
2021-11-22 13:23:25 +0000

    Code cleanup

--- src/test/timer_test.c
+++ src/test/timer_test.c
@@ -29,15 +29,16 @@
 #include "drivers/timer/Timer_public.h"
 
 /* Defines when the 0.20s one shot timers will be fired */
-Event fire_one_shot_timers(Event *timer_one_shot_event, int size) {
+Event fire_one_shot_timers(Event *timer_one_shot_event, int amount) {
 
     /* Start a 0.20s one shot timer (For testing) */
+    // TODO: Check if the for loop is causing it run out of timers, then use a function that will only activate a one short timer when the other timer has completed.
     DEBUG_INF("Activating several 0.20s one shot timers");
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < amount; i++) {
+        /* Checking for errors when the timer is initialized */
         bool error = Timer_start_one_shot(0.20, &timer_one_shot_event[i]);
-        if (error = ERROR_NONE) 
-        {
+        if (error = ERROR_NONE) {
+            DEBUG_INF("Could not initialize 0.20s one shot timer");
             Debug_exit(1);
         }
 
@@ -70,8 +71,7 @@ int main(void) {
     /* Init system critical modules */
     Kernel_init_critical_modules();
 
-    if (!Debug_init()) 
-    {
+    if (!Debug_init()) {
         return 0;
     }
 
@@ -80,53 +80,48 @@ int main(void) {
     /* Required initialisations and verifications for the Datapool, EventManager, and Board */
     DEBUG_INF("Init DP...");
     DataPool_init();
-    if (!DP.INITIALISED)
-    {
+    if (!DP.INITIALISED) {
         DEBUG_INF("Could not initialize the datapool, exiting...");
         Debug_exit(1);
     }
 
     DEBUG_INF("Init EventManager...");
-    if (!EventManager_init())
-    {
+    if (!EventManager_init()) {
         DEBUG_INF("Could not initialize EventManager");
         Debug_exit(1);
     }
 
     DEBUG_INF("Init Board...");
     Board_init();
-    if (!DP.BOARD_INITIALISED)
-    {
+    if (!DP.BOARD_INITIALISED) {
         DEBUG_INF("Could not initialize the Board");
         Debug_exit(1);
     }
 
     /* Init the timer module and check for errors within the timer module start-up */
     error = Timer_init();
-    if (error != ERROR_NONE)
-    {
+    if (error != ERROR_NONE) {
+        DEBUG_INF("Could not initialize the Timer");
         Debug_exit(1);
     }
 
     DEBUG_INF("Activating a 1s one shot timer to verify simple functionality");
     error = Timer_start_one_shot(1, &timer_1_one_shot_done);
-    if (error != ERROR_NONE)
-    {
+    if (error != ERROR_NONE) {
+        DEBUG_INF("Could not initialize the 1s one shot timer");
         Debug_exit(1);
     }
 
     DEBUG_INF("Starting a 1s periodic timer");
     error = Timer_start_periodic(1, &timer_1_periodic_done);
-    if (error != ERROR_NONE)
-    {
+    if (error != ERROR_NONE) {
+        DEBUG_INF("Could not initialize the 1s periodic timer");
         Debug_exit(1);
     }
 
     /* Run main loop */
-    while (run_loop)
-    {
-        if (EventManager_poll_event(timer_1_periodic_done))
-        {
+    while (run_loop) {
+        if (EventManager_poll_event(timer_1_periodic_done)) {
             /* Keep track how many 1s timers were fired */
             num_of_1_timers++;
         }
@@ -137,13 +132,11 @@ int main(void) {
             timer_disabled = true;
         }
 
-        if (EventManager_poll_event(timer_0_20_done))
-        {
+        if (EventManager_poll_event(timer_0_20_done)) {
             num_of_0_20_fired_timers++;
         }
 
-        if (num_of_0_20_fired_timers >= one_shot_timers_amount)
-        {
+        if (num_of_0_20_fired_timers >= one_shot_timers_amount) {
             run_loop = false;
         }
 
