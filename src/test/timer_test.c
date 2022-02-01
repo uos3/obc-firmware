ffcd4c66f78e7849b67745cd14df4effb9cd391d
kCbg <gianborgo@hotmail.com>
2022-02-01 21:00:31 +0000

    Fixed some != signs and error code

--- src/test/timer_test.c
+++ src/test/timer_test.c
@@ -140,7 +140,7 @@ int main(void) {
             /* Initializing a 0.2s one shot timer every second*/
             /* Checking for errors when the timer is initialized */
             bool error = Timer_start_one_shot(0.20, &timer_0_20_done);
-            if (error = ERROR_NONE) {
+            if (error != ERROR_NONE) {
                 DEBUG_INF("Could not initialize 0.20s one shot timer");
                 Debug_exit(1);
             }
