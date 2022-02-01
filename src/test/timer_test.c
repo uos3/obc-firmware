aa521a548bdde82c49109823295485bf3727d66a
kCbg <gianborgo@hotmail.com>
2022-02-01 13:14:52 +0000

    Bug fixes

--- src/test/timer_test.c
+++ src/test/timer_test.c
@@ -28,23 +28,11 @@
 #include "system/event_manager/EventManager_public.h"
 #include "drivers/timer/Timer_public.h"
 
-/* Defines when the 0.20s one shot timers will be fired */
-Event fire_one_shot_timers(Event *timer_one_shot_event, int amount) {
+/* -------------------------------------------------------------------------   
+ * CONSTANTS
+ * ------------------------------------------------------------------------- */
 
-    /* Start a 0.20s one shot timer (For testing) */
-    // TODO: Check if the for loop is causing it run out of timers, then use a function that will only activate a one short timer when the other timer has completed.
-    DEBUG_INF("Activating several 0.20s one shot timers");
-    for (int i = 0; i < amount; i++) {
-        /* Checking for errors when the timer is initialized */
-        bool error = Timer_start_one_shot(0.20, &timer_one_shot_event[i]);
-        if (error = ERROR_NONE) {
-            DEBUG_INF("Could not initialize 0.20s one shot timer");
-            Debug_exit(1);
-        }
-
-        DEBUG_INF("Timer event code: 0x%04X", timer_one_shot_event[i]);
-    }
-}
+#define TIMERS_AMOUNT 24
 
 /* -------------------------------------------------------------------------   
  * MAIN
@@ -54,19 +42,19 @@ int main(void) {
 
     ErrorCode error = ERROR_NONE;
     bool run_loop = true;
-    /* Amount of one shot timers that will be initialised */
-    int one_shot_timers_amount = 24;
     /* This will be an array of events fired every 0.20s by the one shot timer */
-    Event timer_0_20_done[one_shot_timers_amount];
+    Event timer_0_20_done;
+    //Event timer_0_20_done;
     /* This will be the event fired every second to test periodic timers */
     Event timer_1_periodic_done;
     /* Starting one shot timer to verify simple functionality of the system */
     Event timer_1_one_shot_done;
     bool timer_disabled = false;
     int num_of_0_20_fired_timers = 0;
+    int num_of_1_one_shot_timers = 0;
     int num_of_1_timers = 0;
     /* This will be used to measure how long should the 1s periodic timer should run for */
-    int periodic_run_time_required = 10;
+    int periodic_run_time_required = 20;
 
     /* Init system critical modules */
     Kernel_init_critical_modules();
@@ -75,7 +63,9 @@ int main(void) {
         return 0;
     }
 
+    DEBUG_INF("---------------------");
     DEBUG_INF(" ---- TIMER TEST ----");
+    DEBUG_INF("---------------------");
     
     /* Required initialisations and verifications for the Datapool, EventManager, and Board */
     DEBUG_INF("Init DP...");
@@ -85,11 +75,15 @@ int main(void) {
         Debug_exit(1);
     }
 
+    DEBUG_INF("");
+
     DEBUG_INF("Init EventManager...");
     if (!EventManager_init()) {
         DEBUG_INF("Could not initialize EventManager");
         Debug_exit(1);
     }
+
+    DEBUG_INF("");
 
     DEBUG_INF("Init Board...");
     Board_init();
@@ -98,12 +92,16 @@ int main(void) {
         Debug_exit(1);
     }
 
+    DEBUG_INF("");
+
     /* Init the timer module and check for errors within the timer module start-up */
     error = Timer_init();
     if (error != ERROR_NONE) {
         DEBUG_INF("Could not initialize the Timer");
         Debug_exit(1);
     }
+
+    DEBUG_INF("");
 
     DEBUG_INF("Activating a 1s one shot timer to verify simple functionality");
     error = Timer_start_one_shot(1, &timer_1_one_shot_done);
@@ -112,6 +110,8 @@ int main(void) {
         Debug_exit(1);
     }
 
+    DEBUG_INF("");
+
     DEBUG_INF("Starting a 1s periodic timer");
     error = Timer_start_periodic(1, &timer_1_periodic_done);
     if (error != ERROR_NONE) {
@@ -119,29 +119,57 @@ int main(void) {
         Debug_exit(1);
     }
 
+    DEBUG_INF("");
+
     /* Run main loop */
     while (run_loop) {
+
+        // Check if timer of 0.2s has polled.
+        if (EventManager_poll_event(timer_0_20_done)) {
+            DEBUG_INF("Polled 0.2s one shot timer");
+            num_of_0_20_fired_timers++;
+        }
+
+        if (EventManager_poll_event(timer_1_one_shot_done)) {
+            num_of_1_one_shot_timers++;
+            DEBUG_INF("Simple functionality test for a 1s one shot timer");
+        }
+        
+
         if (EventManager_poll_event(timer_1_periodic_done)) {
+            /* Initializing a 0.2s one shot timer every second*/
+            /* Checking for errors when the timer is initialized */
+            bool error = Timer_start_one_shot(0.20, &timer_0_20_done);
+            if (error = ERROR_NONE) {
+                DEBUG_INF("Could not initialize 0.20s one shot timer");
+                Debug_exit(1);
+            }
+            DEBUG_INF("Timer event code: 0x%04X", timer_0_20_done);
             /* Keep track how many 1s timers were fired */
             num_of_1_timers++;
         }
 
+        /* TODO: Make it so that the periodic timer is running for the amount of time required to launch a single one shot timer everytime the periodic timer is triggered.
+         * It will run for a specific amount of time until either the num_of_0_20_fired_timers >= TIMERS_AMOUNT or until it has reached a maximum run time */
         if (num_of_1_timers >= periodic_run_time_required && !timer_disabled) {
-            /* Disable all timers to enable the one shot timers to be enabled */
-            fire_one_shot_timers(timer_0_20_done, one_shot_timers_amount);
+
+            /* Disable the 1s periodic timer after it ran its required time */
+            error = Timer_disable(timer_1_periodic_done);
+            if (error != ERROR_NONE) {
+                DEBUG_INF("Error disabling the timer exiting...");
+                Debug_exit(1);
+            }
             timer_disabled = true;
+            run_loop = false;
         }
 
-        if (EventManager_poll_event(timer_0_20_done)) {
-            num_of_0_20_fired_timers++;
-        }
-
-        if (num_of_0_20_fired_timers >= one_shot_timers_amount) {
+        if (num_of_0_20_fired_timers >= TIMERS_AMOUNT) {
             run_loop = false;
         }
 
         EventManager_cleanup_events();
     }
+    DEBUG_INF("The test ran for a total of %ds. In %ds had a total of %dx0.2s timers and %dx1s one shot timer.", periodic_run_time_required, num_of_1_timers, num_of_0_20_fired_timers, num_of_1_one_shot_timers);
     
     /* Disable all timers then destroy the event manager. Must be done in this
      * order to prevent a hardfault if a timer attempts to access the event
