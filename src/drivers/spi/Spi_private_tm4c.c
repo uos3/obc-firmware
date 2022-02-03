2408e8fa9653bf051a3f5ad9a090fc3139a1eb8c
kCbg <gianborgo@hotmail.com>
2022-02-03 12:42:53 +0000

    Just initial commit for the SPI driver.

---
+++ src/drivers/spi/Spi_private_tm4c.c
@@ -0,0 +1,16 @@
+/**
+ * @file Spi_private_tm4c.c
+ * @author Gianluca Borgo (gb2g20@soton.ac.uk/gianborgo@hotmail.com)
+ * @brief This is the private SPI driver for the cubesat's firmware.
+ * 
+ * 
+ * 
+ * @version 0.1
+ * @date 2021-10-22
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
+/* -------------------------------------------------------------------------   
+ * INCLUDES
+ * ------------------------------------------------------------------------- */
