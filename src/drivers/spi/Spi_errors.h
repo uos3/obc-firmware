faf1be0b6368e6f6332204f3e20959361c7a3f74
kCbg <gianborgo@hotmail.com>
2022-04-20 14:35:55 +0100

    Added comments to SPI error codes

--- src/drivers/spi/Spi_errors.h
+++ src/drivers/spi/Spi_errors.h
@@ -22,14 +22,33 @@
  * DEFINES
  * ------------------------------------------------------------------------- */
 
+/**
+ * @brief The I2C driver has not been initialised.
+ */
 #define SPI_ERROR_NOT_INITIALISED ((ErrorCode)MOD_ID_SPI | 1)
 
+/**
+ * @brief Failed to enable an I2C peripheral.
+ */
 #define SPI_ERROR_SPI_PERIPH_ENABLE_FAILED ((ErrorCode)MOD_ID_SPI | 2)
 
+/**
+ * @brief Failed to enable a GPIO peripheral.
+ */
 #define SPI_ERROR_GPIO_PERIPH_ENABLE_FAILED ((ErrorCode)MOD_ID_SPI | 3)
 
-#define SPI_ERROR_NOT_INITIALISED ((ErrorCode)MOD_ID_SPI | 4)
+/**
+ * @brief Attempted to send 0 bytes.
+ */
+#define SPI_ERROR_ZERO_LENGTH_SEND ((ErrorCode)MOD_ID_SPI | 4)
 
-#define SPI_ERROR_ZERO_LENGTH_SEND ((ErrorCode)MOD_ID_SPI | 5)
+/**
+ * @brief An unexpected action step index was found while executing an
+ * action.
+ */
+#define SPI_ERROR_UNEXPECTED_ACTION_STEP ((ErrorCode)MOD_ID_SPI | 5)
 
-#define SPI_ERROR_UNEXPECTED_ACTION_STEP ((ErrorCode)MOD_ID_SPI | 6)
+/**
+ * @brief MISO failed to go to low on a given number of checks so the clock stability can't be checked
+ */
+#define SPI_ERROR_MISO_NOT_LOW ((ErrorCode)MOD_ID_SPI | 6)
